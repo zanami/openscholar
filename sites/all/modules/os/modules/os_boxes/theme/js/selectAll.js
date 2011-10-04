0fa8cb794b60bae34fbc7bdf2644298b956a1845
MPetrovic <mpetrovic@iq.harvard.edu>
2011-10-04 17:05:18 -0400

    Select All checkbox works properly.

--- sites/all/modules/os/modules/os_boxes/theme/js/selectAll.js
+++ sites/all/modules/os/modules/os_boxes/theme/js/selectAll.js
@@ -7,7 +7,7 @@ Drupal.behaviors.selectAllStart = function (ctx) {
 		var stop = this.name.lastIndexOf('['),
 			name = this.name.slice(0,stop),
 			chk = this.checked,
-			$cbxs = $('input[name|='+name+']');
+			$cbxs = $('input[name^="'+name+'"]');
 		$cbxs.each(function() {
 			this.checked = chk;
 		});
