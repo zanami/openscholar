3867b2bedaca925723d205b024cb2d1c866dee38
Dan Cabral <iqssdev@dhcp-0019769008-7c-ac.client.fas.harvard.edu>
2011-10-19 17:09:34 -0400

    Add new widget: wobble effect

--- sites/all/modules/openscholar_vsite/vsite_layout/vsite_layout_ui/theme/vsite_layout_ui.js
+++ sites/all/modules/openscholar_vsite/vsite_layout/vsite_layout_ui/theme/vsite_layout_ui.js
@@ -51,6 +51,13 @@ Drupal.behaviors.scholarlayout = function() {
   
   //compress widgets widths to fit in the space provided
   vsite_layout_setWidgetAutoWidth();
+  
+  //Create the wobble func
+  vsite_layout_init_wobble();
+  
+  //Shake the new widget
+  alert('alert');
+  $('.changed').vibrate('y', 5, 20, 85);
 };
 
 function scholarlayout_add_removal_hooks() {
@@ -334,4 +341,34 @@ function vsite_layout_setWidgetAutoWidth(){
 			items.width(nWidth - 51);
 		}//If this is a skinny container
 	});
+}
+
+////////wobble///////////
+function vsite_layout_init_wobble(){	
+jQuery.fn.vibrate = function(axis, distance, repetition, duration) {
+    
+    var i = 0;
+    var o = distance / distance;
+    
+    switch(axis) {
+    	case 'x':
+    		while(i < repetition) {
+    			$(this).animate({marginLeft:'-'+distance+'px'}, duration);
+    			$(this).animate({marginLeft:distance}, duration);
+    			i++;
+    			if(i == repetition) {
+    				$(this).animate({marginLeft:o}, duration);
+    			}
+    		}
+    		break;
+    	
+    	case 'y':
+    		while(i < repetition) {
+    			$(this).animate({marginTop:'-'+distance+'px'}, duration);
+    			$(this).animate({marginTop:distance}, duration);
+    			i++;
+    		}
+    		break;
+    }
+}
 }
