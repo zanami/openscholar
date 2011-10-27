e4b892b946caf061f815b26cd1ab1e1252395c43
Richard Brandon <richard.brandon@gmail.com>
2011-10-27 15:28:35 -0400

    Revert "Only create hover events on area of research once."
    This reverts commit 8bb57f55e26b1e79f89f601298dba5f535094e76.

--- sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
+++ sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
@@ -1,28 +1,28 @@
 //Javascript to manipulate the Areas of Interest block on Garys Home Page
 Drupal.behaviors.iqss_gking = function() {
 
-  if (! $.data( $('.front #block-iqss_gking-areas_of_research .col dt span').get(0), 'events' )) {
-    iqss_gking_research_group_tabify();
+  iqss_gking_research_group_tabify();
   
-    iqss_gking_taxonomy_alter_sort_links();
-  
-    $('.front #block-iqss_gking-areas_of_research .col dt span').hover(function() {
-      //Rem 1 for Sticky
-    	window.clearTimeout(timer_block_iqss_gking_areas_of_research);
-    
-      $('.front #block-iqss_gking-areas_of_research dt.active').removeClass('active');
-    	$('.front #block-iqss_gking-areas_of_research dd').fadeOut('fast');
-    	$('.front #block-iqss_gking-areas_of_research .more').hide();
-    
-      $(this).parent('dt').addClass('active');
-      $(this).parent('dt').next('dd').fadeIn('fast');
+  iqss_gking_taxonomy_alter_sort_links();
 
-    });
+  $('.front #block-iqss_gking-areas_of_research .col dt span').hover(function() {
+    //Rem 1 for Sticky
+	window.clearTimeout(timer_block_iqss_gking_areas_of_research);
 
-    iqss_gking_areas_of_research_load_next();
+  $('.front #block-iqss_gking-areas_of_research dt.active').removeClass('active');
+	$('.front #block-iqss_gking-areas_of_research dd').fadeOut('fast');
+	$('.front #block-iqss_gking-areas_of_research .more').hide();
 
-    var timer_block_iqss_gking_areas_of_research = null;
-  }
+    $(this).parent('dt').addClass('active');
+
+    $(this).parent('dt').next('dd').fadeIn('fast');
+
+  });
+
+  iqss_gking_areas_of_research_load_next();
+
+  var timer_block_iqss_gking_areas_of_research = null;
+
 
 };
 
