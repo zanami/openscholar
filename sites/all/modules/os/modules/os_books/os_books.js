302f32bde2d379d964ad1a8623fe9a90a097f17a
MPetrovic <mpetrovic@iq.harvard.edu>
2011-11-02 12:38:35 -0400

    Contextual links for books change when using js swap. Fix for HTML-escaped characters in titles.

--- sites/all/modules/os/modules/os_books/os_books.js
+++ sites/all/modules/os/modules/os_books/os_books.js
@@ -3,7 +3,7 @@
  * or the book links in the content area and the actual contents of a book page
  */
 (function($){
-	var display, header, content = {}, menu, perma;
+	var display, header, content = {}, active, perma;
 	
 	Drupal.behaviors.os_book_linkage = function() {
 		if (!$('body.node-type-book').length) return;
@@ -12,9 +12,7 @@
 		var blocks = $().not('*');
 		perma = $('#book-permalink');
 		header = $('#content-main .title').not('.book-menu .title');
-		// store the DOM elements so we can keep an attributes
-		// and click handlers
-		menu = $('.book-menu')[0];
+		active = $('#content-main .node').attr('id').replace('node-','');
 		
 		pages.each(function(index, elem){
 			// get the nid of the content
@@ -23,7 +21,7 @@
 			// save it to the content object
 			if (typeof content[nid] == 'undefined') {
 				var $elem = $(elem),
-					title = $elem.find('h1:first').remove().html();
+					title = $elem.find('h1:first').remove().text();
 				
 				// add nid as attribute of links with same title
 				$('.block a:contains("'+title+'"), .book-menu a:contains("'+title+'")').each(function (index, elem) {
@@ -52,16 +50,10 @@
 			perma.attr('href', e.target.getAttribute('href'));
 			e.preventDefault();
 			
-			// when the book links are in the content area, 
-			// the nids can't be added to the link
-			$('.book-menu a', display).each(function () {
-				if (!this.getAttribute('nid')) {
-					for (var i in content) {
-						if (content[i].title == (this.innerText || this.textContent)) {
-							this.setAttribute('nid', i);
-						}
-					}
-				}
+			// change the admin contextual links
+			var reg = new RegExp(active, "g");
+			display.parents('.node').find('.ctools-dropdown-container a').each (function () {
+				this.href = this.href.replace(reg, nid);
 			});
 		}
 	}
