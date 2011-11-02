8916bd7b854ed6be01c17ec5cb9871a582e6e4f0
MPetrovic <mpetrovic@iq.harvard.edu>
2011-11-02 11:41:08 -0400

    The table of contents doesn't swap out when in the body of a node.

--- sites/all/modules/os/modules/os_books/os_books.js
+++ sites/all/modules/os/modules/os_books/os_books.js
@@ -3,7 +3,7 @@
  * or the book links in the content area and the actual contents of a book page
  */
 (function($){
-	var display, header, content = {}, nids = {}, perma;
+	var display, header, content = {}, menu, perma;
 	
 	Drupal.behaviors.os_book_linkage = function() {
 		if (!$('body.node-type-book').length) return;
@@ -11,7 +11,10 @@
 		display = $('#content p').not('.book-page p').parent();
 		var blocks = $().not('*');
 		perma = $('#book-permalink');
-		header = $('#content-main .title');
+		header = $('#content-main .title').not('.book-menu .title');
+		// store the DOM elements so we can keep an attributes
+		// and click handlers
+		menu = $('.book-menu')[0];
 		
 		pages.each(function(index, elem){
 			// get the nid of the content
@@ -20,18 +23,18 @@
 			// save it to the content object
 			if (typeof content[nid] == 'undefined') {
 				var $elem = $(elem),
-					title = $elem.find('h1:first').hide().html();
+					title = $elem.find('h1:first').remove().html();
 				
 				// add nid as attribute of links with same title
 				$('.block a:contains("'+title+'"), .book-menu a:contains("'+title+'")').each(function (index, elem) {
 					elem.setAttribute('nid', nid);
-					var parents = $(elem).parents('.content');
+					var parents = $(elem).parents('.block, .book-menu');
 					blocks.add(parents);
 				});
-				content[nid] = {title: title, content: $elem.html()};
+				content[nid] = {title: title, content: $elem.hide().html()};
 			}
 			else {
-				content[nid].content += $(elem).html();
+				content[nid].content += $(elem).hide().html();
 			}
 		});
 		
@@ -42,7 +45,9 @@
 	function toc_click(e) {
 		var nid = e.target.getAttribute('nid');
 		if (content[nid]) {
-			display.hide().html(content[nid].content).fadeIn();
+			$('*:not(.book-menu, .book-menu *)', display).remove();
+			display.append(content[nid].content);
+			$('*:not(.book-menu,.book-menu *)', display).fadeIn();
 			header.html(content[nid].title);
 			perma.attr('href', e.target.getAttribute('href'));
 			e.preventDefault();
