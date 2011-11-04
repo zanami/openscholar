63bc8f0efb2ccadf4995962ef4ac5b918eca9bf4
MPetrovic <mpetrovic@iq.harvard.edu>
2011-11-04 16:13:16 -0400

    JS for tabbed widgets form is working.

--- sites/all/modules/os/modules/os_modalframe/theme/os_modal_tabbed.js
+++ sites/all/modules/os/modules/os_modalframe/theme/os_modal_tabbed.js
@@ -2,12 +2,18 @@
  * 
  */
 $(document).ready(function () {
+	// initialize the tabs and the sortable
 	$('#tabs').tabs({
-				disabled: [$('#tabs .links .new').index()]
+				// keep the new_tab link from being a tab
+				disabled: [$('#tabs .links li').length-1],
+				// use fieldset instead of div
+				panelTemplate: '<fieldset class="panel"></fieldset>',
+				selected: $('#edit-default').val(),
 			  })
 			  .find('.ui-tabs-nav')
 			  .sortable({
 				axis: 'x',
+				// don't let them sort the new_tab
 				items: 'li:not(.new_tab)',
 				update: updateWeight,
 			  });
@@ -17,19 +23,34 @@ $(document).ready(function () {
 		Drupal.modalFrameChild.triggerParentEvent('childResize');
 	}
 	
+	// update the weights of the tabs
+	// should end up being unique
 	function updateWeight() {
 		$('#tabs .links:not(.new_tab) a').each(function (i) {
 			$(this.href).find('.tab-weight').val(i);
 		});
 	}
 	
-	$('.panel .make-default').click(function () {
-		console.log('click default');
+	// set which tab displays by default
+	$('.panel .make-default').live('click', function () {
 		$('#edit-default').val($(this).attr('panel'));
 	});
 	
+	// update the title of the tab
+	$('.panel .tab-title').live('change', function () {
+		var $this = $(this),
+			id = $this.parents('.panel').attr('id');
+		$('#tabs .links a[href="#'+id+'"]').text($this.val());
+	});
+	
+	// add a new tab when the new_tab is clicked on
 	$('#tabs .links .new a').click(function () {
-		console.log('click new');
-		$('#tabs').tabs('add', '#test1', 'New Tab', $('#tabs .links .new').index());
+		var id = 'tab-'+($('#tabs .links li').length-1);
+		$('#tabs').tabs('add', '#'+id, 'New Tab', $('#tabs .links li').length-1);
+
+		// get the full html, including tags of the panel 
+		// and replace the automatically generated panel with it
+		$('#'+id).html($('#tab-new').html().replace(/tab-new/g, id));
+		$('tabs').tabs("select", id);
 	});
 });
