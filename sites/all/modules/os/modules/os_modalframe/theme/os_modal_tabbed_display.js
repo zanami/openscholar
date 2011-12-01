1b41e53bd8992bc6d38ef9d3e509c83dddc1f8df
Richard Brandon <richard.brandon@gmail.com>
2011-12-01 17:02:11 -0500

    if there is no stored active tab use the default

--- sites/all/modules/os/modules/os_modalframe/theme/os_modal_tabbed_display.js
+++ sites/all/modules/os/modules/os_modalframe/theme/os_modal_tabbed_display.js
@@ -11,9 +11,11 @@ Drupal.behaviors.quickTabsRemember = function (ctx) {
 			$tab = $this.find('.qtab-'+active);
 		
 		// we set the class AND click because the order behaviors run in is uncertain
-		$this.find('li.active').removeClass('active');
-		$tab.addClass('active').find('a').click();
-		$this.find('ul.quicktabs_tabs:first li a').unbind('click', clickHandle).click(clickHandle);
+		if($tab.length){
+		  $this.find('li.active').removeClass('active');
+		  $tab.addClass('active').find('a').click();
+		  $this.find('ul.quicktabs_tabs:first li a').unbind('click', clickHandle).click(clickHandle);
+		}
 	});
 	
 	function clickHandle(e) {
