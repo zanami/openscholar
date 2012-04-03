e0d2cf9cacb9e758f0ffcdcdf46959042cc069f7
Richard Brandon <richard.brandon@gmail.com>
2012-04-03 15:40:05 -0400

    obvuscate email addresses

--- sites/all/themes/openscholar/scholar_base/js/scholar_base.js
+++ sites/all/themes/openscholar/scholar_base/js/scholar_base.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-  
+  $('a.show_hidden_email').each(function(){
+	  $(this).attr('href','mailto:'+$(this).attr('email_user')+'@'+$(this).attr('email_domain')).html('email');
+  });
 });
