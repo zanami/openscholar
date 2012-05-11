529b6e2950a2746f1a7135e169f4a5d96d0ebfd9
MPetrovic <mpetrovic@iq.harvard.edu>
2012-05-11 16:40:05 -0400

    Render unused widgets.

--- profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-unused_widgets.tpl.php
+++ profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-unused_widgets.tpl.php
@@ -1,1 +1,4 @@
-<?php
+<div id="edit-layout-unused-widgets">
+<!-- Do something about tabs and factories -->
+<?php echo $children; ?>
+</div>
--- profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-widget.tpl.php
+++ profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-widget.tpl.php
@@ -1,4 +1,5 @@
 <div id="<?php echo $bid; ?>" class="<?php echo implode(' ', $classes_array); ?>">
+	<?php echo $widget_title; ?>
 	<div class="widget-controls">
 		<?php if ($can_delete) {
 			print ctools_modal_text_button("Delete", $w['block_delete_path'], "Delete widget", "delete ctools-modal-openscholar-style-default");
