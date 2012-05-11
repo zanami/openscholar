f8e83dbbe56f68befcf3a8b602ece9dc952f98e5
MPetrovic <mpetrovic@iq.harvard.edu>
2012-05-11 17:00:41 -0400

    Add tags to template.

--- profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-unused_widgets.tpl.php
+++ profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-unused_widgets.tpl.php
@@ -1,4 +1,15 @@
 <div id="edit-layout-unused-widgets">
 <!-- Do something about tabs and factories -->
-<?php echo $children; ?>
+<?php
+if (count($tags) > 0) { ?>
+	<ul><?php
+  foreach ($tags as $t) {
+    ?>
+    <li><a href="#<?php echo $t; ?>"><?php echo $t; ?></a></li>
+    <?php
+  }
+  ?></ul><?php
+}
+echo $children;
+?>
 </div>
