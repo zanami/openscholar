b15039e0d0db2ee03417197540bb9aa74b28020d
Matt Petrovic <mpetrovic@iq.harvard.edu>
2012-05-22 16:34:02 -0400

    Change classes on section messages as objects are dragged around.

--- profiles/openscholar/modules/cp/modules/cp_menu/cp_menu.form.js
+++ profiles/openscholar/modules/cp/modules/cp_menu/cp_menu.form.js
@@ -6,6 +6,7 @@
  *   2. Removes the 'hidden' class when the user selects a new menu from the select.
  */
 (function($) {
+var drag;
 
 function changeSelect() {
   var $this = $(this.oldRowElement),
@@ -14,6 +15,7 @@ function changeSelect() {
     select = $this.find('.menu-name');
   
   select.val(val);
+  emptySections();
 }
 
 function changeRegion() {
@@ -23,6 +25,36 @@ function changeRegion() {
   }).parents('tr').removeClass('hidden');
   
   // move the field to the new region
+  
+  
+  emptySections();
+}
+
+/** 
+ * Deal with the empty section message.
+ * If there no links in the section, switch to the region-empty class
+ * Otherwise, switch to region-populated
+ */
+function emptySections() {
+  console.log(drag);
+  var $table = $(drag.table),
+  // get all the section messages
+    $sections = $('.section-message');
+  
+  // loop through each select
+  // find it's section header, then look down for the section message
+  $('select.menu-name', $table).each(function () {
+    var $header = $(this).parents('tr').prevAll('.section-heading').first(),
+      $message = $header.nextAll('.section-message').first();
+    
+    // remove this message from the list we made earlier
+    $sections = $sections.not($message);
+    $message.removeClass('section-empty').addClass('section-populated');
+  });
+  
+  // at this point, the $sections should only contain 
+  // section messages in sections that are empty
+  $sections.removeClass('section-populated').addClass('section-empty');
 }
   
 Drupal.behaviors.cp_menu_form = {
@@ -30,7 +62,7 @@ Drupal.behaviors.cp_menu_form = {
     // remove the 'hidden' class when a menu is changed
     $('select.menu-name', ctx).change(changeRegion);
     
-    var drag = Drupal.tableDrag['edit-menu-table'];
+    drag = Drupal.tableDrag['edit-menu-table'];
     drag.onDrop = changeSelect;
   }
 };
