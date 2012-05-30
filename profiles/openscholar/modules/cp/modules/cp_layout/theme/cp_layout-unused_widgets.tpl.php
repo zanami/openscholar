3436485b37bed5ae3e0f1f8f12895b7b953179a8
Dan Cabral <rcabral@dhcp-10-249-26-70.fas.harvard.edu>
2012-05-30 14:56:57 -0400

    CP layout style prpogress

--- profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-unused_widgets.tpl.php
+++ profiles/openscholar/modules/cp/modules/cp_layout/theme/cp_layout-unused_widgets.tpl.php
@@ -1,9 +1,8 @@
-<div id="edit-layout-unused-widgets">
 <!-- Do something about tabs and factories -->
 <?php
 echo $factory_html;
 if (count($tags) > 0) { ?>
-	<ul><?php
+	<ul id="widget-categories"><?php
   foreach ($tags as $t) {
     ?>
     <li><a href="#<?php echo $t; ?>"><?php echo $t; ?></a></li>
@@ -12,9 +11,11 @@ if (count($tags) > 0) { ?>
   ?></ul><?php
 }
 ?>
+<div id="edit-layout-unused-widgets">
   <div class="widget-container">
 <?php
 echo $children;
 ?>
   </div>
 </div>
+<div id="websiteLabelTab"></div>
