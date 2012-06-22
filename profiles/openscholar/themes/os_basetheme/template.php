a82099039a0b86c788e6a1522bfa8bddeb8093a9
Dan Cabral <rcabral@dhcp-10-249-17-42.fas.harvard.edu>
2012-06-22 11:55:36 -0400

    Adding osbasetheme template.php file

---
+++ profiles/openscholar/themes/os_basetheme/template.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
+ * 1. Rename each function and instance of "adaptivetheme_subtheme" to match
+ *    your subthemes name, e.g. if your theme name is "footheme" then the function
+ *    name will be "footheme_preprocess_hook". Tip - you can search/replace
+ *    on "adaptivetheme_subtheme".
+ * 2. Uncomment the required function to use.
+ */
+
+/**
+ * // Adds classes to the body tag.
+ */
+function os_basetheme_preprocess_page(&$vars) {
+
+  //Adds OpenScholar header region awareness to body classes
+  $regions = array (
+    'left' => $vars['three_col_first'],
+    'main' => $vars['three_col_second'],
+    'right' => $vars['three_col_third'],
+  );
+
+  $non_empty_regions = array_filter($regions, "__scholar_base_is_empty");
+  $header_classes = 'header-';
+  if (count($non_empty_regions)) {
+    $header_classes .= implode('-', array_keys($non_empty_regions));
+   }
+  else {
+    $header_classes .= 'none';
+  }
+
+  $classes[] = $header_classes;
+
+  $vars['body_classes'] = implode(' ', $classes);
+}
+
