7b635054694c9d0e13572c6ab4c6459eadd22935
Richard Brandon <Richard.Brandon@gmail.com>
2012-08-02 17:04:51 -0400

    fuzzy Date

--- profiles/openscholar/modules/contrib/google_feedapi/theme/google_feedapi.js
+++ profiles/openscholar/modules/contrib/google_feedapi/theme/google_feedapi.js
@@ -25,9 +25,14 @@ Drupal.behaviors.googleFeedAPI = {
 		           	  var entry = result.feed.entries[i];
 		           	  
 		           	  var date = "";
-		           	  if(typeof entry.publishedDate != 'undefined'){
+		           	  if(typeof entry.publishedDate != 'undefined' && entry.publishedDate != ''){
 		           		//@todo find a good way to do FuzzyTime in js
-		           		date = entry.publishedDate;
+		           		date = googleFeedAPIFuzzyDate(entry.publishedDate);
+		           		if(typeof date == 'undefined'){
+		           		  date = "";
+		           		}else{
+		           		  date = "<span class='date'>" + date + "</span>";
+		           		}
 		           	  }
 		           	  
 		           	  var content = entry.content;
@@ -54,3 +59,26 @@ Drupal.behaviors.googleFeedAPI = {
 };
 
 })(jQuery);
+
+
+//Takes an ISO time and returns a string representing how
+//long ago the date represents.
+function googleFeedAPIFuzzyDate(time){
+	
+	var date = new Date(time),
+		diff = (((new Date()).getTime() - date.getTime()) / 1000),
+		day_diff = Math.floor(diff / 86400);
+			
+	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+		return;
+			
+	return day_diff == 0 && (
+			diff < 60 && "just now" ||
+			diff < 120 && "1 minute ago" ||
+			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+			diff < 7200 && "1 hour ago" ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+		day_diff == 1 && "Yesterday" ||
+		day_diff < 7 && day_diff + " days ago" ||
+		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+}
