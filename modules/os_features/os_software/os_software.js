566fe90bd9db68dbb5d5527a501b4af64d01a99a
oren <oren_robinson@harvard.edu>
2012-08-22 11:24:29 -0400

    add rbuild-specific short name field to os_software
    also adapted show/hide logic to include the second field #155

--- modules/os_features/os_software/os_software.js
+++ modules/os_features/os_software/os_software.js
@@ -7,15 +7,17 @@
   Drupal.behaviors.os_software = {
     attach: function(context){
       var inputSelector = '#edit-field-software-method-und';
-      var urlFieldSelector = '#field-software-repo-add-more-wrapper';
-      
+      var urlFieldSelector1 = '#field-software-repo-add-more-wrapper';
+      var urlFieldSelector2 = '#field-rbuild-short-name-add-more-wrapper';
       // Toggles Repo URL visible or hidden based on new selection
       $(inputSelector).change(function(){
         var newValue = $(this).val().toLowerCase();
-        if (newValue === 'manual upload') {
-          $(urlFieldSelector).hide();
+        if (newValue !== 'rbuild repository') {
+          $(urlFieldSelector1).hide();
+          $(urlFieldSelector2).hide();
         } else {
-          $(urlFieldSelector).show();
+          $(urlFieldSelector1).show();
+          $(urlFieldSelector2).show();
         }
       }).trigger('change'); // Triggers change to initialize field visibility
     }
