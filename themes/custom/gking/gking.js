c0fd114bf95e5c33fb15e6be37fe9d6f081f498d
Dan Cabral <rcabral@huit-00906.local>
2012-09-07 11:16:45 -0400

    Progress to Garys theme: frontpage layout is working, frontpage large photo with bio blurb/more link, starting applying css to tabs--will finish today. taxonomy is large piece of site that is missing.#98

--- themes/custom/gking/gking.js
+++ themes/custom/gking/gking.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
 
-    $('.front .region-header-second .box-os_boxes_simple div.boxes-box-content p:last').append('<a class="more" href="#">More</a>');
+    jQuery('.front .region-header-second .block-boxes-os_boxes_html .boxes-box div:last').append('<a class="more" href="#">More</a>');
 
     var container = $("body.front .region-header-second #block-boxes-scholar_biocv_bio");
     container.find('div.node').prepend('<a class="more" href="#">CLOSE X</a>');
     container.find('div.node .content').prepend('<h3 class="cv-direct-download">Full CV: <a href="http://gking.harvard.edu/vitae/vitae.pdf">PDF</a></h3><div class="clear"></div>');
 
 
-    $(".front .region-header-second a.more").click(function(event){
+    jQuery(".front .region-header-second a.more").click(function(event){
       container.find('a.more').toggle();
       if (container.is(".bio-open")) {
         container.removeClass("bio-open").animate({height:'0'},"1500");
@@ -16,8 +16,8 @@ $(document).ready(function() {
       }
     });
     
-    $(".front #pub_type_tabs").tabs({ fx: { opacity: 'toggle' } });
+    jQuery(".front #pub_type_tabs").tabs({ fx: { opacity: 'toggle' } });
     
-    $(".front #header-top .wrap").addClass('clearfix');
+    jQuery(".front #header-top .wrap").addClass('clearfix');
     
 });
