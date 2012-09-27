b511528cfb7de4b456ec77a9e911fdad39df6d60
oren <oren_robinson@harvard.edu>
2012-09-27 16:27:46 -0400

    restore "areas of research" hover effect #434
    i'm noticing a little slowness on my computer, but the effect is the
    same as production.

--- themes/custom/gking/gking.js
+++ themes/custom/gking/gking.js
@@ -6,25 +6,30 @@
     attach: function (context) {
 
       /**
-       * Prepares CV overlay (PDF) for "More" link on front page header.
+       * Prepares Bio overlay for front page header.
        */
-      $('.front .region-header-second .block-boxes-os_boxes_html .boxes-box-content p:last')
-        .append('<a class="more" href="#">More</a>');
+      // Bio set to display: none by default, for browsers without js.
       var container = $(".front .region-header-second .block-boxes-os_boxes_bio");
-      // Style set to display: none by default, for browsers without js.
-      container.toggle();
-      container.find('.node')
-        .prepend('<a class="more" href="#">CLOSE X</a>');
-      container.find('.node .node-content')
-        .prepend('<h3 class="cv-direct-download">Full CV: <a href="http://gking.harvard.edu/vitae/vitae.pdf">PDF</a></h3><div class="clear"></div>');
+      if (container.length) {
+        container.toggle();
+      
+        // Adds necessary more/close links to show/hide bio node content.
+        $('.front .region-header-second .block-boxes-os_boxes_html .boxes-box-content p:last')
+          .append('<a class="more" href="#">More</a>');
+        container.find('.node')
+          .prepend('<a class="more" href="#">CLOSE X</a>');
+        container.find('.node .node-content')
+          .prepend('<h3 class="cv-direct-download">Full CV: <a href="http://gking.harvard.edu/vitae/vitae.pdf">PDF</a></h3><div class="clear"></div>');
 
-      $(".front .region-header-second a.more").click(function (event) {
-        if (container.hasClass("bio-open")) {
-          container.removeClass("bio-open").animate({height: '0'}, "1500");
-        } else {
-          container.addClass("bio-open").animate({height: '70%'}, "1500");
-        }
-      });
+        // Both the "More" and "Close X" links trigger this animate event.
+        $(".front .region-header-second a.more").click(function (event) {
+          if (container.hasClass("bio-open")) {
+            container.removeClass("bio-open").animate({height: '0'}, "1500");
+          } else {
+            container.addClass("bio-open").animate({height: '70%'}, "1500");
+          }
+        });
+      }
   
       /**
        * Prepares the People page with tabs.
@@ -59,33 +64,31 @@
 //        $(this).toggleClass("line");
 //        $(this).next("div.description").toggle();
 //      });
-//      // Original method
-//      var areas = $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content ul li ul li:not(.aor-processed)');
-//      if (areas.length) {
-//        // Setup the AOR block
-//        $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content ul li ul li:not(.aor-processed)')
-//          .hover(function () {
-//          $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content ul li ul li.active')
-//            .removeClass('active');
-//          $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content ul li ul li div.description')
-//            .fadeOut('fast');
-//          $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content .more')
-//            .hide();
-//          $(this)
-//            .addClass('active');
-//          $(this)
-//            .find('div.description')
-//            .fadeIn('fast');
-//        });
-//        $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content ul li.last ul li:not(.aor-processed)')
-//          .filter(":first")
-//          .each(function (index) {
-//          $(this)
-//            .hover();
-//        });
-//        $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content ul li ul li')
-//          .addClass('aor-processed');
-//      }
+      // Original method
+      var $sel = '#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content';
+      var areas = $($sel + ' ul li ul li:not(.aor-processed)');
+      if (areas.length) {
+        // Setup the AOR block
+        $($sel + ' ul li ul li:not(.aor-processed)').hover(function () {
+          $($sel + ' ul li ul li.active')
+            .removeClass('active');
+          $($sel + ' ul li ul li div.description')
+            .fadeOut('fast');
+          $($sel + ' .more')
+            .hide();
+          $(this)
+            .addClass('active')
+            .find('div.description')
+            .fadeIn('fast');
+        });
+        $($sel + ' ul li.last ul li:not(.aor-processed)')
+          .filter(":first")
+          .each(function (index) {
+            $(this).hover();
+        });
+        $('#content-column .block-boxes-os_taxonomy_fbt .boxes-box-content ul li ul li')
+          .addClass('aor-processed');
+      }
       
       /**
        * Scrolls to the top of the page when you click the sort links
