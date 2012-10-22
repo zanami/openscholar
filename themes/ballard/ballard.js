9a2b9d3b3030b28c50d5431f074e60b84c6c8903
Dan Cabral <rcabral@dhcp-140-247-184-42.fas.harvard.edu>
2012-10-22 15:28:46 -0400

    add context to aberdeen and ballard jquery scripts

--- themes/ballard/ballard.js
+++ themes/ballard/ballard.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 //Insert the secondary nav to the content region for smartphone
  jQuery(window).resize(function() {
   if (jQuery(window).width() < 600) {
-     jQuery("#block-os-secondary-menu").insertAfter(".region-sidebar-second");
+     jQuery(".region-header-third #block-os-secondary-menu").insertAfter(".region-sidebar-second");
   }
 
  });
