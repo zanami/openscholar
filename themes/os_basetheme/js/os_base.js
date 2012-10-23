b25ddb23ce231287de36cc38d92dd768dc0fb400
Dan Cabral <rcabral@dhcp-140-247-184-42.fas.harvard.edu>
2012-10-23 16:03:35 -0400

    Adding js file to base. Including js so that IE8 recognizes html5 elements.

---
+++ themes/os_basetheme/js/os_base.js
@@ -0,0 +1,11 @@
+<!--[if lt IE 9]>
+<script>
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('footer');
+document.createElement('hgroup');
+</script>
+<![endif]-->
