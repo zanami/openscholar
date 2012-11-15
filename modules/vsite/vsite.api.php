05a21a46e30981ed8577f33ced677bd392b816c5
Roy Segall <roy.segall@gizra.com>
2012-11-15 13:28:48 +0200

    Revert "convert hook_vsite_og_node_type_info() to hook_vsite_app_info() #846"
    This reverts commit 0edf43cadcdb952ed2216a315d1bcde03985fc97.

--- modules/vsite/vsite.api.php
+++ modules/vsite/vsite.api.php
@@ -1,5 +1,4 @@
 <?php
-// $Id$
 
 /**
  * @file
@@ -7,40 +6,12 @@
  */
 
 /**
- * Implements hook_vsite_app_info().
+ * Implements hook_vsite_og_node_type_info().
  *
- * Notifies vsite module about OS App configuration.
- *
- * @return array $info
- *   An associative array containing:
- *   - group: (optiona) An array of OG group node types (site types).
- *   - group content: (optional) An array of arrays, keyed by group content
- *     node type machine name.
- *     - NODETYPE: A string machine name matching hook_node_info().
- *       - singluar: A string, translated, human readable name for node type.
- *       - plural: A string, translated, human readable plural for node type.
- *   - restricted groups: (optional) An array of site types that should
- *     not see this app listed.
- *   - main path: (optional) A string, the app's main path menu item.
- *   - settings path: (optional) A string, the app's settings form path.
+ * Notify vsite module which nodes bundle types are group or a group/content.
  */
-function hook_vsite_app_info() {
-  $info = array();
-
-  // Exported app info for OS News.
-  $info['os_news'] = array(
-    'group' => array(),
-    'group content' => array(
-      'news' => array(
-        'singular' => t('news item'),
-        'plural' => t('news items'),
-      ),
-    ),
-    'restricted groups' => array('project', 'department'),
-    'main path' => 'news',
-    'settings path' => 'features/os_news/settings',
+function hook_vsite_og_node_type_info() {
+  return array(
+    'group1' => 'group',
   );
-
-  return $info;
 }
-
