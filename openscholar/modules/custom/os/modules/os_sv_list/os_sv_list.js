8d50f406c155acd105f8794dc4140c98219ef7dd
Amitaibu <amitai@gizra.com>
2012-12-03 12:07:11 +0200

    Update OS

--- openscholar/modules/custom/os/modules/os_sv_list/os_sv_list.js
+++ openscholar/modules/custom/os/modules/os_sv_list/os_sv_list.js
@@ -6,38 +6,65 @@
 (function ($) {
   Drupal.behaviors.os_sv_list = {
     attach: function(context) {
+      //when content type changes, update sorting options list.
+    	$('#os_sv_list_content_type').change(function() { 
+    	  sortby = $('#edit-sort-by');
+    	  whitelist = ['sort_newest', 'sort_oldest', 'sort_alpha'];
+    	  content_type = $('#os_sv_list_content_type').attr('value'); 
+    	  selected_sort = 'sort_' + content_type; 
+    	  
+    	  sortby.children('option').each(function() { //why you no function?
+    	  	this_sort = $(this).attr('value');
+    	  	if ($.inArray(this_sort, whitelist) == -1) {    	  		
+    	  		//show/hide appropriate options
+    	  		remove = (this_sort != selected_sort);
+    	  		$(this).attr('hidden', remove).attr('disabled', remove);
+    	  		
+    	  		//deselect invalidated option
+    	  		if (remove && ($(this).parent().attr('value') == this_sort)) {
+    	  			$(this).parent().attr('value', whitelist[0]);
+    	  		}
+    	  		
+    	  		//for new boxes, select special case as default
+    	  		if (!remove && Drupal.settings.os_sv_list_box.new_box) {
+    	  			$(this).parent().attr('value', this_sort);
+    	  		}
+    	  	}
+    	  });
+    	});
+
       // Get the default value of the content_type.
-      var content_type = $('#os_sv_list_content_type').val(); 
+      var content_type = $('#os_sv_list_content_type').val();
       var show_all_checked = $('#biblio_show_all_check').is(':checked')?true:false;
-      
+
  	  // Check for a select action on the conten_type.
       $('#os_sv_list_content_type', context).change(function() {
         content_type = $(this).val();
       });
-      
+
       // Check for a click event on the show all publication types.
       $('#biblio_show_all_check', context).click(function() {
         if($('#biblio_show_all_check').not(':checked') && content_type == 'biblio') {
-          $('#os_sv_list_biblio_whitelist').show(); 
-          show_all_checked = false;	
+          $('#os_sv_list_biblio_whitelist').show();
+          show_all_checked = false;
         }
         if($('#biblio_show_all_check').is(':checked')) {
           $('#os_sv_list_biblio_whitelist').hide();
           show_all_checked = true;
         }
       });
-      
+
       // Hide/show the select all pub types based on select value.
       if(content_type == 'biblio') {
    	    $('#os_sv_list_biblio_show_all').show('slow',function(){
    	      if(show_all_checked == false) {
    	    	$('#os_sv_list_biblio_whitelist').show();
    	      }else {
-   	    	$('#os_sv_list_biblio_whitelist').hide();  
+   	    	$('#os_sv_list_biblio_whitelist').hide();
    	      }
    	    });
    	  }else {
-   		$('#os_sv_list_biblio_show_all').hide();  
+   		$('#os_sv_list_biblio_show_all').hide();
    		$('#os_sv_list_biblio_whitelist').hide();
    	  }
 	}
