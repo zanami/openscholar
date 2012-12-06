78148baa554c330c87ffa05e2fc3a13074a76fa1
Roy Segall <roy.segall@gizra.com>
2012-12-06 13:28:33 +0200

    #1072: Add mechanism to test Widgets.

--- openscholar/behat/features/bootstrap/FeatureContext.php
+++ openscholar/behat/features/bootstrap/FeatureContext.php
@@ -5,13 +5,31 @@ use Behat\Behat\Context\Step\Given;
 use Behat\Gherkin\Node\TableNode;
 use Guzzle\Service\Client;
 use Behat\Behat\Context\Step;
+use Behat\Behat\Context\Step\When;
 
 require 'vendor/autoload.php';
 
 class FeatureContext extends DrupalContext {
 
-  // Variable to pass into the last xPath expression.
+  /**
+   * Variable to pass into the last xPath expression.
+   */
   private $xpath = '';
+
+  /**
+   * The box delta we need to hide.
+   */
+  private $box = '';
+
+  /**
+   * Hold the user name and password for the selenium tests for log in.
+   */
+  private $drupal_users;
+
+  /**
+   * Hold the NID of the vsite.
+   */
+  private $nid;
 
   /**
    * Initializes context.
@@ -24,6 +42,10 @@ class FeatureContext extends DrupalContext {
   public function __construct(array $parameters) {
     if (isset($parameters['drupal_users'])) {
       $this->drupal_users = $parameters['drupal_users'];
+    }
+
+    if (isset($parameters['vsite'])) {
+      $this->nid = $parameters['vsite'];
     }
   }
 
@@ -115,17 +137,6 @@ class FeatureContext extends DrupalContext {
    * @Then /^I should print page$/
    */
   public function iShouldPrintPage() {
-    $this->getSession()->visit($this->locatePath('/user'));
-    $element = $this->getSession()->getPage();
-    $element->fillField($this->getDrupalText('username_field'), 'admin');
-    $element->fillField($this->getDrupalText('password_field'), 'admin');
-    $submit = $element->findButton($this->getDrupalText('log_in'));
-    if (empty($submit)) {
-      throw new \Exception('No submit button at ' . $this->getSession()->getCurrentUrl());
-    }
-
-    // Log in.
-    $submit->click();
     $element = $this->getSession()->getPage();
     print_r($element->getContent());
   }
@@ -216,4 +227,108 @@ class FeatureContext extends DrupalContext {
     );
   }
 
+  /**
+   * @Given /^I publish a new blog entry$/
+   */
+  public function iPublishANewBlogEntry() {
+    return array(
+      new Step\When('I fill in "Comment" with "Lorem ipsum john doe"'),
+      new Step\When('I press "Save"'),
+    );
+  }
+
+
+  /**
+   * @Given /^the widget "([^"]*)" is set in the "([^"]*)" page with the following <settings>:$/
+   */
+  public function theWidgetIsSetInThePageWithSettings($page, $widget, TableNode $table) {
+    $code = "os_migrate_demo_set_box_in_region({$this->nid}, '$page', '$widget');";
+    $this->box[] = $this->getDriver()->drush("php-eval \"{$code}\"");
+    $hash = $table->getRows();
+
+    list($box, $delta, $context) = explode(",", $this->box[0]);
+
+    $metasteps = array();
+    // @TODO: Don't use the hard coded address - remove john from the address.
+    $metasteps[] = new Step\When('I visit "john/os/widget/boxes/' . $delta . '/edit"');
+
+    // @TODO: Use XPath to fill the form instead of giving the type of the in
+    // the scenario input.
+    foreach ($hash as $form_elements) {
+      switch ($form_elements[2]) {
+        case 'select list':
+          $metasteps[] = new Step\When('I select "' . $form_elements[1] . '" from "'. $form_elements[0] . '"');
+          break;
+        case 'checkbox':
+          $metasteps[] = new Step\When('I '. $form_elements[1] . ' the box "' . $form_elements[0] . '"');
+          break;
+        case 'textfield':
+          $metasteps[] = new Step\When('I fill in "' . $form_elements[0] . '" with "1"');
+          break;
+        case 'radio':
+          $metasteps[] = new Step\When('I select the radio button "' . $form_elements[0] . '" with the id "' . $form_elements[1] . '"');
+          break;
+      }
+    }
+
+    $metasteps[] = new Step\When('I press "Save"');
+    $metasteps[] = new Step\When('I clear the cache');
+
+    return $metasteps;
+  }
+
+  /**
+   * @Given /^the widget "([^"]*)" is set in the "([^"]*)" page$/
+   */
+  public function theWidgetIsSetInThePage($page, $widget) {
+    $code = "os_migrate_demo_set_box_in_region({$this->nid}, '$page', '$widget');";
+    $this->box[] = $this->getDriver()->drush("php-eval \"{$code}\"");
+    $this->getDriver()->drush("cc all");
+  }
+
+  /**
+   * @When /^I assign the node "([^"]*)" to the term "([^"]*)"$/
+   */
+  public function iAssignTheNodeToTheTerm($node, $term) {
+    $code = "os_migrate_demo_assign_node_to_term('$node', '$term');";
+    $this->getDriver()->drush("php-eval \"{$code}\"");
+  }
+
+  /**
+   * Hide the boxes we added during the scenario.
+   *
+   * @AfterScenario
+   */
+  public function afterScenario($event) {
+    if (empty($this->box)) {
+      return;
+    }
+
+    // Loop over the box we collected in the scenario, hide them and delete
+    // them.
+    foreach ($this->box as $box_handler) {
+      $data = explode(',', $box_handler);
+      $code = "os_migrate_demo_hide_box({$this->nid}, '{$data[0]}', '{$data[1]}', '{$data[2]}');";
+      $this->getDriver()->drush("php-eval \"{$code}\"");
+    }
+  }
+
+  /**
+   * @Given /^I should see the following <links>$/
+   */
+  public function iShouldNotSeeTheFollowingLinks(TableNode $table) {
+    $page = $this->getSession()->getPage();
+    $hash = $table->getRows();
+
+    foreach ($hash as $i => $table_row) {
+      if (empty($table_row)) {
+        continue;
+      }
+      $element = $page->find('xpath', "//a[.='{$table_row[0]}']");
+
+      if (empty($element)) {
+        throw new Exception(printf("The link %s wasn't found on the page", $table_row[0]));
+      }
+    }
+  }
 }
---
+++ openscholar/behat/features/comments.feature
@@ -0,0 +1,10 @@
+Feature:
+  Testing the comment publishing for a blog entry.
+
+  @api @current
+  Scenario: Check that all of the apps are turned on
+    Given I am logged in as a user with the "administrator" role
+     When I visit "john/blog"
+      And I click "First blog"
+      And I publish a new blog entry
+     Then I should see "Lorem ipsum john doe"
