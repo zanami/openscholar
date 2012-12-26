1c293bff9f4ab046dd24aeeb09a03803d5ead49f
Roy Segall <roy.segall@gizra.com>
2012-12-26 10:57:14 +0200

    Add debug scenario for travis.

--- openscholar/behat/features/activity_stream.feature
+++ openscholar/behat/features/activity_stream.feature
@@ -3,6 +3,6 @@ Feature:
 
   @api
   Scenario: Check activity stream page
-    Given I visit "/activity"
+    Given I debug the page "/activity"
      Then I should see "John created Class Material: Who was JFK?"
 
--- openscholar/behat/features/bootstrap/FeatureContext.php
+++ openscholar/behat/features/bootstrap/FeatureContext.php
@@ -338,4 +338,21 @@ class FeatureContext extends DrupalContext {
   public function iSleepFor($seconds) {
    sleep($seconds);
   }
+
+  /**
+   * @Given /^I debug the page "([^"]*)"$/
+   */
+  public function iDebugThePage($path) {
+    $url = $this->locatePath($path);
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    print_r($data);
+    $this->getSession()->visit($this->locatePath($path));
+  }
 }
