080be41e56b3f95a5affdb5d81e35a261dd7babe
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-01-14 15:36:27 -0500

    Added news-date option to sv list where content type = news.  #1418
    Also fixed some javascript.  It was filtering the sort types when the content type select changed, but was failing to do that when the page loaded, so you ended up with all the sorts available until the select got changed.

--- modules/os/modules/os_sv_list/os_sv_list.js
+++ modules/os/modules/os_sv_list/os_sv_list.js
@@ -8,50 +8,53 @@
     attach: function(context) {
       //when content type changes, update sorting options list.
       var old_type = $('#os_sv_list_content_type').val();
-    	$('#os_sv_list_content_type').change(function() { 
-    	  var sortby = $('#edit-sort-by'),
-    	      whitelist = ['sort_newest', 'sort_oldest', 'sort_alpha'],
-    	      content_type = $('#os_sv_list_content_type').val(), 
-    	      selected_sort = 'sort_' + content_type,
-    	      more_link = $('#edit-more-link'),
-    	      defaults = Drupal.settings.more_link_defaults;
-    	  
-    	  sortby.children('option').each(function() { //why you no function?
-    	  	this_sort = $(this).attr('value');
-    	  	if ($.inArray(this_sort, whitelist) == -1) {    	  		
-    	  		//show/hide appropriate options
-    	  		remove = (this_sort != selected_sort);
-    	  		$(this).attr('hidden', remove).attr('disabled', remove);
-    	  		
-    	  		//deselect invalidated option
-    	  		if (remove && ($(this).parent().attr('value') == this_sort)) {
-    	  			$(this).parent().attr('value', whitelist[0]);
-    	  		}
-    	  		
-    	  		//for new boxes, select special case as default
-    	  		if (!remove && Drupal.settings.os_sv_list_box.new_box) {
-    	  			$(this).parent().attr('value', this_sort);
-    	  		}
-    	  	}
-    	  });
-    	  
-    	  // swap out the more link url
-    	  if (more_link.val()) {
-    	    defaults[old_type] = more_link.val();
-    	  }
-    	  more_link.val(defaults[content_type]);
-    	  old_type = content_type;
-    	  
+  	  $('#os_sv_list_content_type').change(function() { 
+    	var sortby = $('#edit-sort-by');
+    	var whitelist = ['sort_newest', 'sort_oldest', 'sort_alpha'];
+    	var content_type = $('#os_sv_list_content_type').val();
+    	var selected_sort = 'sort_' + content_type;
+    	var more_link = $('#edit-more-link');
+    	var defaults = Drupal.settings.more_link_defaults;
+	  
+    	sortby.children('option').each(function() { //why you no function?
+    	  this_sort = $(this).attr('value');
+    	  if ($.inArray(this_sort, whitelist) == -1) {    	  		
+	  		//show/hide appropriate options
+	  		remove = (this_sort != selected_sort);
+	  		$(this).attr('hidden', remove).attr('disabled', remove);
+	  		
+	  		//deselect invalidated option
+	  		if (remove && ($(this).parent().attr('value') == this_sort)) {
+	  			$(this).parent().attr('value', whitelist[0]);
+	  		}
+	  		
+	  		//for new boxes, select special case as default
+	  		if (!remove && Drupal.settings.os_sv_list_box.new_box) {
+	  			$(this).parent().attr('value', this_sort);
+	  		}
+	  	  }
     	});
+    	  
+	    // swap out the more link url
+	    if (more_link.val()) {
+	      defaults[old_type] = more_link.val();
+	    }
+	    more_link.val(defaults[content_type]);
+	    old_type = content_type;
+    	  
+      });
+  	  
+  	  //perform the change callback once now.
+  	  $('#os_sv_list_content_type').change(); 
 
       // Get the default value of the content_type.
       var content_type = $('#os_sv_list_content_type').val();
-      var show_all_checked = $('#biblio_show_all_check').is(':checked')?true:false;
+      var show_all_checked = $('#biblio_show_all_check').is(':checked') ? true : false;
 
  	  // Check for a select action on the conten_type.
-      $('#os_sv_list_content_type', context).change(function() {
-        content_type = $(this).val();
-      });
+//      $('#os_sv_list_content_type', context).change(function() {
+//        content_type = $(this).val();
+//      });
       /*
       // Check for a click event on the show all publication types.
       $('#biblio_show_all_check', context).click(function() {
