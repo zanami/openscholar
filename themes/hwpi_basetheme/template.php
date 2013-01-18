b341327d98bc0405dbabab1264d6afbe33c01aa3
Jeff Burnz <jeff@adaptivethemes.com>
2013-01-18 14:04:46 +0100

    fix colors in header, footer, backgrounds, style page nav menu

--- themes/hwpi_basetheme/template.php
+++ themes/hwpi_basetheme/template.php
@@ -1,9 +1,13 @@
 <?php
 
 // Rebuild the theme data.
-//system_rebuild_theme_data();
-//drupal_theme_rebuild();
+system_rebuild_theme_data();
+drupal_theme_rebuild();
 
+
+/**
+ * Preprocess variables for html.tpl.php
+ */
 function hwpi_basetheme_preprocess_html(&$vars) {
 
   $scripts = array(
@@ -16,4 +20,39 @@ function hwpi_basetheme_preprocess_html(&$vars) {
     load_subtheme_script('js/' . $script, 'hwpi_basetheme', 'header', $weight = NULL);
   }
 
+}
+
+/**
+ * Returns HTML for a menu link and submenu.
+ *
+ * Adaptivetheme overrides this to insert extra classes including a depth
+ * class and a menu id class. It can also wrap menu items in span elements.
+ *
+ * @param $vars
+ *   An associative array containing:
+ *   - element: Structured array data for a menu link.
+ */
+function hwpi_basetheme_menu_link(array $vars) {
+  $element = $vars['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+
+  if (!empty($element['#original_link'])) {
+    if (!empty($element['#original_link']['depth'])) {
+      $element['#attributes']['class'][] = 'menu-depth-' . $element['#original_link']['depth'];
+    }
+    if (!empty($element['#original_link']['mlid'])) {
+      $element['#attributes']['class'][] = 'menu-item-' . $element['#original_link']['mlid'];
+    }
+  }
+
+  if (isset($element['#localized_options']) && !empty($element['#localized_options']['attributes']['title'])) {
+    unset($element['#localized_options']['attributes']['title']);
+  }
+
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>";
 }
