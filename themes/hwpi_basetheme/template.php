a44085fd44e0f3db88c2e62ed94d2bea3fdbf46f
Jeff Burnz <jeff@adaptivethemes.com>
2013-01-21 19:04:30 +0100

    add styles for content types classes, links, events

--- themes/hwpi_basetheme/template.php
+++ themes/hwpi_basetheme/template.php
@@ -20,7 +20,45 @@ function hwpi_basetheme_preprocess_html(&$vars) {
     load_subtheme_script('js/' . $script, 'hwpi_basetheme', 'header', $weight = NULL);
   }
 
+  // debug message
+  //$message = 'Blog entry Blog Post Elements Cheat Sheet has been updated.';
+  //drupal_set_message($message, 'status');
+  //drupal_set_message($message, 'warning');
+  //drupal_set_message($message, 'error');
 }
+
+
+/**
+ * Preprocess variables for comment.tpl.php
+ */
+function hwpi_basetheme_preprocess_node(&$vars) {
+  //dsm($vars);
+
+  if($vars['node']->type == 'event') {
+    $vars['event_start'] = array();
+    if(isset($vars['field_date'][0]['value']) && !empty($vars['field_date'][0]['value'])) {
+      date_default_timezone_set($vars['field_date'][0]['timezone']);
+      $event_start_date = strtotime($vars['field_date'][0]['value']);
+      $vars['event_start']['month'] = check_plain(date('M', $event_start_date));
+      $vars['event_start']['day'] = check_plain(date('d', $event_start_date));
+      $vars['classes_array'][] = 'event-start';
+    }
+  }
+}
+
+
+/**
+ * Preprocess variables for comment.tpl.php
+ */
+function hwpi_basetheme_preprocess_comment(&$vars) {
+  if($vars['new']) {
+    $vars['title'] = $vars['title'] . '<em class="new">' . $vars['new'] . '</em>';
+    $vars['new'] = '';
+  }
+}
+
+
+
 
 /**
  * Returns HTML for a menu link and submenu.
@@ -56,6 +94,3 @@ function hwpi_basetheme_menu_link(array $vars) {
   $output = l($element['#title'], $element['#href'], $element['#localized_options']);
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>";
 }
-
-
-
