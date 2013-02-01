8932db2b4329dbdf6d50cd1ed780ee968fdece16
oren <get.oren@gmail.com>
2013-02-01 16:24:34 -0500

    add global ga settings form, add ga link tracking #746

--- modules/os/modules/os_ga/os_ga.js
+++ modules/os/modules/os_ga/os_ga.js
@@ -1,1 +1,4 @@
+/**
+ * Extends Google analytics settings to track more links.
+ */
 jQuery.extend(Drupal.settings,{"googleanalytics": {"trackOutgoing": 1,"trackMailto": 1,"trackDownload": 1,"trackDownloadExtensions": "7z|aac|arc|arj|asf|asx|avi|bin|csv|doc|exe|flv|gif|gz|gzip|hqx|jar|jpe?g|js|mp(2|3|4|e?g)|mov(ie)?|msi|msp|pdf|phps|png|ppt|qtm?|ra(m|r)?|sea|sit|tar|tgz|torrent|txt|wav|wma|wmv|wpd|xls|xml|z|zip"}});
