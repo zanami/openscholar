aecc4e29dde0e4118f0ca6cfb14b00778ad36bcb
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-02-07 21:19:23 -0500

    Slideshow - Added javascript bindings for flexslider controls. #1557
    Replaced controls with spans and .click bindings.  So far so good.  Pager is next.

--- modules/os_features/os_slideshow/os_slideshow_controls.tpl.php
+++ modules/os_features/os_slideshow/os_slideshow_controls.tpl.php
@@ -4,4 +4,9 @@
  */
 ?>
 
-<div id="flex-controls"></div>
+<div id="flex-controls">
+  <span id="flex-prev">prev</span>
+  <span id="flex-pause">pause</span> 
+  <div id="flex-pager"></div>
+  <span id="flex-next">next</span>
+</div>
--- modules/os_features/os_slideshow/os_slideshow_flexslider.js
+++ modules/os_features/os_slideshow/os_slideshow_flexslider.js
@@ -10,10 +10,31 @@
 Drupal.behaviors.os_slideshow = {
   attach: function(context, settings) {
     $(window).load(function() {
-      //$('.flexslider').flexslider(Drupal.settings.os_slideshow.flexslider); //there  should be an of sliders?  then we can loda them by class and ssetting.
       for (delta in Drupal.settings.os_slideshow) {
+        //start slideshow
         var div = 'div#boxes-box-' + delta;
-        $(div + ' .flexslider').flexslider(Drupal.settings.os_slideshow[delta])
+        var slider = $(div + ' .flexslider');
+        slider.flexslider(Drupal.settings.os_slideshow[delta])
+
+        //bind controls 
+        slider.find('#flex-next').click(function() {slider.flexslider('next');});
+        slider.find('#flex-prev').click(function() {slider.flexslider('prev');});
+        slider.find('#flex-pause').click(function() {
+          if (slider.hasClass('pause')) {
+            slider.removeClass('pause').flexslider('play');
+          } else {
+            slider.addClass('pause').flexslider('pause');
+          }
+        });
+
+        //make pager
+        var text = '';
+        for (var i=0; i<slider.find('img').length; i++) {
+          text += '<span class="flex-pager-item '+i+'">o</span>';
+        }
+        slider.find('#flex-pager').append(text);
+        
+        //bind pager
       }
     });
   }
