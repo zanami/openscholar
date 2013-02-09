836a4d74aabbffa21754f760fff28fbc09f746d4
Dan Cabral <rcabral@huit-00906.home>
2013-02-09 18:25:38 -0500

    Add "Section Nav depth class" function to template.php. Add better default section nav css to os base and remove messy/bulky section nav css from ballard.#1577

--- themes/os_basetheme/template.php
+++ themes/os_basetheme/template.php
@@ -91,3 +91,38 @@ function os_basetheme_preprocess_overlay(&$vars) {
   // we never want these. They look awful
  $vars['tabs'] = false;
 }
+
+/**
+ * Returns HTML for a menu link and submenu.
+ *
+ * Adaptivetheme overrides this to insert extra classes including a depth
+ * class and a menu id class. It can also wrap menu items in span elements.
+ *
+ * @param $vars
+ *   An associative array containing:
+ *   - element: Structured array data for a menu link.
+ */
+function os_basetheme_menu_link(array $vars) {
+  $element = $vars['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+
+  if (!empty($element['#original_link'])) {
+    if (!empty($element['#original_link']['depth'])) {
+      $element['#attributes']['class'][] = 'menu-depth-' . $element['#original_link']['depth'];
+    }
+    if (!empty($element['#original_link']['mlid'])) {
+      $element['#attributes']['class'][] = 'menu-item-' . $element['#original_link']['mlid'];
+    }
+  }
+
+  if (isset($element['#localized_options']) && !empty($element['#localized_options']['attributes']['title'])) {
+    unset($element['#localized_options']['attributes']['title']);
+  }
+
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>";
+}
