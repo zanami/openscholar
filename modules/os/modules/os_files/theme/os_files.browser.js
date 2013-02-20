3902f3570ae12b178fcc21bc0c7f6398dff931ec
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-02-20 16:17:28 -0500

    #1730: Create blank pane to the right of the media browser. Fill it with file edit form when a file is selected.

---
+++ modules/os/modules/os_files/theme/os_files.browser.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+(function ($, undefined) {
+  Drupal.behaviors.osFilesMediaBrowser = {
+    attach: function (ctx, s) {
+      // do nothing on ajax that have no browser
+      if ($(ctx).find('#media-browser-library-list').length == 0) return;
+      
+      var $items = $('#media-browser-library-list li .media-item', ctx),
+          forms = {};
+      
+      $items.each(function () {
+        var base = this.id,
+            fid = this.getAttribute('data-fid'),
+            settings = {
+              url: s.basePath+'file/'+fid+'/edit/nojs',
+              event: 'click',
+              wrapper: 'file-edit-no-selection',
+              progress: {type: 'throbber'}
+            },
+            ajax = new Drupal.ajax(base, this, settings);
+      });
+    }
+  };
+})(jQuery, undefined);
