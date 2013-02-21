e12a359af088e1848c3ce9561086d19044eb3e4c
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-02-21 13:50:17 -0500

    Fix people double title in sidebar.  #1636
    The sidebar_teaser view mode for person nodes is all set.  Just needs styling.

--- themes/hwpi_basetheme/template.php
+++ themes/hwpi_basetheme/template.php
@@ -42,7 +42,7 @@ function hwpi_basetheme_preprocess_node(&$vars) {
 function hwpi_basetheme_process_node(&$vars) {
   // Event persons, change title markup to h1
   if ($vars['type'] == 'person') {
-    if (!$vars['teaser']) {
+    if (!$vars['teaser'] && $vars['view_mode'] != 'sidebar_teaser') {
       $vars['title_prefix']['#suffix'] = '<h1 class="node-title">' . $vars['title'] . '</h1>';
     } 
   }
