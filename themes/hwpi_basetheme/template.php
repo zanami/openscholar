8299f3de37efc8e68a24d01c0486bd6aa87bea7b
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-02-26 17:10:21 -0500

    #1546: Dates are always correct in colorbox

--- themes/hwpi_basetheme/template.php
+++ themes/hwpi_basetheme/template.php
@@ -428,9 +428,8 @@ function hwpi_basetheme_status_messages($vars) {
 
 function hwpi_basetheme_date_formatter_pre_view_alter(&$entity, $vars) {
   // only display the start time for this particular instance of a repeat event
-  if (!isset($entity->view)) {
-    $entity->view = views_get_current_view();
-  }
+  $entity->view = views_get_current_view();
+
   if (isset($entity->view) && isset($entity->view->row_index) && isset($entity->view->result[$entity->view->row_index])) {
     $result = $entity->view->result[$entity->view->row_index];
     $field = 'field_data_field_date_field_date_value';
@@ -442,5 +441,8 @@ function hwpi_basetheme_date_formatter_pre_view_alter(&$entity, $vars) {
       }
     }
     $entity->date_id = 'node.'.$entity->nid.'.field_date.'.$delta;
+  }
+  else {
+    $entity->active_date = $vars['items'][0];
   }
 }
