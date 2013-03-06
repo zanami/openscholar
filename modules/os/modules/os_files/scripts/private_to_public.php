f62bb834b6877e4af4d20d0e20f2d33f3ffd9347
Roy Segall <roy.segall@gizra.com>
2013-03-06 09:39:36 +0200

    Updating the person field photo as well.

--- modules/os/modules/os_files/scripts/private_to_public.php
+++ modules/os/modules/os_files/scripts/private_to_public.php
@@ -39,8 +39,11 @@ if (empty($result['node'])) {
   drush_log(dt('No records were found'), 'error');
   return;
 }
-
 $nodes = node_load_multiple(array_keys($result['node']));
+
+// When updating the file field there is a need for flushing the cache. The
+// variable will notify us later if needed.
+$flush_cache = FALSE;
 foreach ($nodes as $node) {
   $wrapper = entity_metadata_wrapper('node', $node);
 
@@ -71,11 +74,48 @@ foreach ($nodes as $node) {
     file_save($file);
 
     drush_log(dt('File @name updated successfully.', array('@name' => $file->filename)), 'success');
+    $flush_cache = TRUE;
   }
+
+  // Handling with the person photo.
+  if (empty($node->field_person_photo[LANGUAGE_NONE][0])) {
+    continue;
+  }
+
+  $personal_photo = $node->field_person_photo[LANGUAGE_NONE][0];
+  if (strpos($personal_photo['uri'], 'private://') !== 0) {
+    drush_log(dt('The person photo(@name) is already public - skipping.', array('@name' => $value['filename'])), 'warning');
+    continue;
+  }
+
+  $file = file_load($personal_photo['fid']);
+
+  // Building the new path of the file.
+  $destination = str_replace('private://', 'public://', $file->uri);
+
+  // Create the destined folder if it's not exists.
+  if (!is_dir(get_destination_dir($destination))) {
+    // Creating the folder.
+    drupal_mkdir($destination_folder, NULL, TRUE);
+  }
+
+  $moved_file = @file_move($file, $destination);
+  if ($moved_file) {
+    drush_log(dt('File @name moved successfully.', array('@name' => $file->filename)), 'success');
+  }
+
+  $file->uri = $moved_file->uri;
+  file_save($file);
+
+  drush_log(dt('File @name updated successfully.', array('@name' => $file->filename)), 'success');
+  $flush_cache = TRUE;
 }
 
 // Flushing caches for rebuild filed data.
-drupal_flush_all_caches();
+if ($flush_cache) {
+  drush_log(dt('Flushing the cache for update the files field data.'), 'warning');
+  drupal_flush_all_caches();
+}
 
 /**
  * Returning the destined path for the file.
