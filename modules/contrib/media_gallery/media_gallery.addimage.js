0d4f58f04b727fc61c739c5d6167530e26b1d12f
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-03-11 15:56:08 -0400

    #869: Prevent overlay from opening when Add media link is clicked.

--- modules/contrib/media_gallery/media_gallery.addimage.js
+++ modules/contrib/media_gallery/media_gallery.addimage.js
@@ -10,6 +10,7 @@ Drupal.media_gallery = {};
 
 Drupal.media_gallery.open_browser = function (event) {
   event.preventDefault();
+  event.stopPropagation();
   var pluginOptions = { 'id': 'media_gallery', 'multiselect' : true , 'types': Drupal.settings.mediaGalleryAllowedMediaTypes};
   Drupal.media.popups.mediaBrowser(Drupal.media_gallery.add_media, pluginOptions);
 };
