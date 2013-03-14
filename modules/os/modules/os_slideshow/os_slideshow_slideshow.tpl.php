0a3cd8522053425fd8319fc9d93465b5773062ab
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-03-14 16:17:43 -0400

    Updated os_slideshow to latest code.  #2054

--- modules/os/modules/os_slideshow/os_slideshow_slideshow.tpl.php
+++ modules/os/modules/os_slideshow/os_slideshow_slideshow.tpl.php
@@ -1,13 +1,15 @@
-<?php
+<?php 
 /*
  * Flexslider slides and controls
  */
 ?>
 
-<div class="flexslider <?php echo $layout; ?>">
-  <ul class="slides">
+<div id="<?php echo 'layout_' . $layout; ?>">
+	<div class="hero">
+  <ul class="rslides">
     <?php print implode("\n", $slides); ?>
   </ul>
-
+  
   <?php print $controls; ?>
+</div>
 </div>
