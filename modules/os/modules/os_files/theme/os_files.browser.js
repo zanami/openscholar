38c5584164bae9310d98052731332763985e514b
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-03-15 16:07:57 -0400

    #1730: Errant console.log

--- modules/os/modules/os_files/theme/os_files.browser.js
+++ modules/os/modules/os_files/theme/os_files.browser.js
@@ -32,7 +32,6 @@
       };
       
       $('#media-browser-tabset').tabs('option', 'select', function (event, ui) {
-        console.log(arguments);
         if (ui.tab.hash) {
           window.location.hash = ui.tab.hash;
         }
