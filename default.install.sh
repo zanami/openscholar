01bcc2109884ce81de038617ca8934f8c55cbb52
Amitaibu <amitai@gizra.com>
2013-03-28 10:40:54 +0200

    Fix default install file.

--- default.install.sh
+++ default.install.sh
@@ -1,4 +1,13 @@
-drush si -y openscholar --account-pass=admin --db-url=mysql://root:root@localhost/os --uri=http://local:8888/os openscholar_flavor_form.os_profile_flavor=development openscholar_install_type.os_profile_type=vsite
-drush vset purl_base_domain http://local:8888/os
+#!/bin/bash
+
+chmod 777 www/sites/default
+rm -rf www/
+mkdir www
+
+bash scripts/build
+cd www
+
+drush si -y openscholar --account-pass=admin --db-url=mysql://root:root@localhost/os --uri=http://localhost/os openscholar_flavor_form.os_profile_flavor=development openscholar_install_type.os_profile_type=vsite
+drush vset purl_base_domain http://localhost/os_make
 drush en -y os_migrate_demo
 drush mi --all --user=1
