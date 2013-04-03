7b2a2a8605c7f06536312a6c266e68c7cbb827da
Richard Brandon <Richard.Brandon@gmail.com>
2013-04-03 15:11:05 -0400

    Prevent multiple click events

--- modules/os/modules/os_files/theme/os_files_upload_form.js
+++ modules/os/modules/os_files/theme/os_files_upload_form.js
@@ -6,15 +6,22 @@ Drupal.behaviors.os_upload_form = {
     var $ = jQuery,
         $input = $('<label for="edit-upload-upload" class="file-select form-submit">Upload</label>'),
         $file_select = $('#edit-upload input[type="file"]', ctx);
-    $file_select.before($input);
+    $file_select.before($input).click(function() {
+      $(this).addClass('clicked');
+    });
     $input.bind('mousedown', function (e) {$input.addClass('focus');})
           .bind('mouseup', function(e) {$input.removeClass('focus');})
-          .bind('click', function(e) {$file_select.click();});
+          .bind('click', function(e) {
+        	if(!$file_select.hasClass('clicked')){
+        	  $file_select.click();
+            }
+          });
     
     function changeHandler (e) {
       if (!('result' in e) || e.result) {
         $('#file-entity-add-upload .form-actions #edit-next', ctx).click();
       }
+      $(this).removeClass('clicked');
     }
     
     $file_select.change(changeHandler).bind('propertychange', changeHandler);
