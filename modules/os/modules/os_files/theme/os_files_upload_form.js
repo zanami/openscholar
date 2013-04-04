72a6c17cf76ab395fdf629986ed3b799699d0a3c
Richard Brandon <Richard.Brandon@gmail.com>
2013-04-04 14:25:51 -0400

    show and add focus for firefox, #2307

--- modules/os/modules/os_files/theme/os_files_upload_form.js
+++ modules/os/modules/os_files/theme/os_files_upload_form.js
@@ -4,24 +4,17 @@
 Drupal.behaviors.os_upload_form = {
   attach: function (ctx) {
     var $ = jQuery,
-        $input = $('<label for="edit-upload-upload" class="file-select form-submit">Upload</label>'),
-        $file_select = $('#edit-upload input[type="file"]', ctx);
-    $file_select.before($input).click(function() {
-      $(this).addClass('clicked');
-    });
-    $input.bind('mousedown', function (e) {$input.addClass('focus');})
-          .bind('mouseup', function(e) {$input.removeClass('focus');})
-          .bind('click', function(e) {
-        	if(!$file_select.hasClass('clicked')){
-        	  $file_select.click();
-            }
-          });
+    $input = $('<label for="edit-upload-upload" class="file-select form-submit">Upload</label>'),
+    $file_select = $('#edit-upload input[type="file"]', ctx);
+    $file_select.before($input);
+    $input.bind('mousedown', function (e) {$file_select.show().focus(); $input.addClass('focus');})
+          .bind('mouseup', function(e) {$file_select.hide(); $input.removeClass('focus').hide();});;
     
     function changeHandler (e) {
       if (!('result' in e) || e.result) {
         $('#file-entity-add-upload .form-actions #edit-next', ctx).click();
       }
-      $(this).removeClass('clicked');
+      //$(this).removeClass('clicked');
     }
     
     $file_select.change(changeHandler).bind('propertychange', changeHandler);
