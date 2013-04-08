4dad98dfe836b9944ba6bf0d0abae46e1fcde9b0
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-04-08 10:35:02 -0400

    #2214: Add Help icon, that displays allowed file types when users mouse over it.

--- modules/os/modules/os_files/theme/os_files_upload_form.js
+++ modules/os/modules/os_files/theme/os_files_upload_form.js
@@ -5,10 +5,13 @@ Drupal.behaviors.os_upload_form = {
   attach: function (ctx) {
     var $ = jQuery,
         $input = $('<label for="edit-upload-upload" class="file-select form-submit">Upload</label>'),
+        $help = $('<div class="form-help"></div>')
         $file_select = $('#edit-upload input[type="file"]', ctx);
     $file_select.before($input);
     $input.bind('mousedown', function (e) {$input.addClass('focus');})
           .bind('mouseup', function(e) {$input.removeClass('focus');});
+     
+    $('.form-item-upload label[for="edit-upload"]', ctx).after($help);
     
     function changeHandler (e) {
       if (!('result' in e) || e.result) {
