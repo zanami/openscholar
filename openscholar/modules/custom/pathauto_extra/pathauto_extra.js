f4dc14605312c270ed475d24d151eff7812b337b
oren <get.oren@gmail.com>
2013-04-22 15:43:36 -0400

    working form & callback; needs integrate w/ purl #871

--- openscholar/modules/custom/pathauto_extra/pathauto_extra.js
+++ openscholar/modules/custom/pathauto_extra/pathauto_extra.js
@@ -37,14 +37,33 @@
                 //add listener to URL path settings.  fetch new path via ajax
                 $('#edit-title').change(function() {
                     if (Drupal.settings.pathauto_extra && Drupal.settings.pathauto_extra.make_alias) {
+                        // Prepares the ajax callback URL to query.
                         var href = href = 'http://' + document.location.host + '/pathauto_extra/alias_js';
+                        // Prepares the URL parameters to call for this node.
                         var data = Drupal.settings.pathauto_extra;
+                        // Cleans the title value stored in our URL params.
                         data.title = $.trim($('#edit-title').attr('value'));
 
                         if (data.title.length > 0 && $('#edit-path-pathauto').attr('checked')) {
+                            // Adds autocomplete-style default Drupal throbber.
+                            $('.form-item-title .description').html('<strong>Link URL:</strong> <span class="ajax-progress"><span class="throbber"></span></span>');
+                            // Fetches the generated alias from the menu callback.
                             $.getJSON(href, data, function(json) {
                                 if (json.status) {
                                     $('#edit-path-alias').attr('value', json.data);
+                                    // @todo add base_url
+                                    var base_url = Drupal.settings.pathauto_extra.prefix;
+                                    var description = '<strong>Link URL:</strong> ' + base_url + json.data + ' <a id="pathauto-extra-edit-path" href="#path[pathauto]">edit</a>';
+
+                                    $('.form-item-title .description').html(description);
+                                    $('.form-item-title .description a').click(function() {
+                                        $('html, body', context).animate({ scrollTop: $('.vertical-tabs').offset().top });
+                                        $('.vertical-tab-button a:contains("URL path settings")', context).click();
+                                        // @todo toggle click trigger to enable input
+                                        $('#edit-path-pathauto').click().attr('checked', false).attr('value', 0);
+                                        $('.form-item-path-alias').removeClass('form-disabled');
+                                        $('#edit-path-alias').removeAttr("disabled").focus().select();
+                                    });
                                 }
                             });
                         }
