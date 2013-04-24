14426cedbc6b7d0b10c8e0050438fa3563a7d179
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-04-24 14:12:17 -0400

    Fixed tabbing.  #2489

--- openscholar/themes/os_basetheme/templates/node--presentation.tpl.php
+++ openscholar/themes/os_basetheme/templates/node--presentation.tpl.php
@@ -52,32 +52,32 @@ if (!$page) {
   <?php endif; // end teaser ?>
   
   <?php if (!$teaser): // begin default adaptivetheme full page node tpl ?>
-  <?php print render($title_prefix); ?>
-  <?php if ($title && !$page): //widgets can display content on its own page?>
-    <header<?php print $header_attributes; ?>>
-      <?php if ($title): ?>
-        <h1<?php print $title_attributes; ?>>
-          <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a>
-        </h1>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php if(!empty($user_picture) || $display_submitted): ?>
-    <footer<?php print $footer_attributes; ?>>
-      <?php print $user_picture; ?>
-      <p class="author-datetime"><?php print $submitted; ?></p>
-    </footer>
-  <?php endif; ?>
-
-  <div<?php print $content_attributes; ?>>
-    <?php print render($content); ?>
-  </div>
-
-  <?php if ($links = render($content['links'])): ?>
-    <nav<?php print $links_attributes; ?>><?php print $links; ?></nav>
-  <?php endif; ?>
+    <?php print render($title_prefix); ?>
+    <?php if ($title && !$page): //widgets can display content on its own page?>
+      <header<?php print $header_attributes; ?>>
+        <?php if ($title): ?>
+          <h1<?php print $title_attributes; ?>>
+            <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a>
+          </h1>
+        <?php endif; ?>
+      </header>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+  
+    <?php if(!empty($user_picture) || $display_submitted): ?>
+      <footer<?php print $footer_attributes; ?>>
+        <?php print $user_picture; ?>
+        <p class="author-datetime"><?php print $submitted; ?></p>
+      </footer>
+    <?php endif; ?>
+  
+    <div<?php print $content_attributes; ?>>
+      <?php print render($content); ?>
+    </div>
+  
+    <?php if ($links = render($content['links'])): ?>
+      <nav<?php print $links_attributes; ?>><?php print $links; ?></nav>
+    <?php endif; ?>
 
   <?php print render($content['comments']); ?>
 
