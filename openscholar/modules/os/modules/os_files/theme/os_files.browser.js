f9b3eec000ef5c83d2e0fecdc9dc1f92fb1150fd
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-04-26 11:13:52 -0400

    #2197: Don't allow filter form to submit the normal way.

--- openscholar/modules/os/modules/os_files/theme/os_files.browser.js
+++ openscholar/modules/os/modules/os_files/theme/os_files.browser.js
@@ -5,35 +5,36 @@
 (function ($, undefined) {
   Drupal.behaviors.osFilesMediaBrowser = {
     attach: function (ctx, s) {
-      if (!$('#file-edit-section', ctx).length) return;
+      if ($('#file-edit-section', ctx).length) {
       
-      var $items = $('.media-item:not(.os-files-processed)', ctx),
-          forms = {};
-      
-      $items.each(function () {
-        var base = this.id,
-            fid = this.getAttribute('data-fid'),
-            settings = {
-              url: s.basePath+('pathPrefix' in s?s.pathPrefix:'')+'file/'+fid+'/edit/nojs',
-              event: 'click',
-              wrapper: 'file-edit-section',
-              method: 'html',
-              progress: {type: 'throbber'}
-            },
-            ajax = new Drupal.ajax(base, this, settings);
-       $(this).addClass('os-files-processed');
-      });
-      
-      // Sometimes the ajax handler will just try to work with #undefined, which is wrong
-      Drupal.ajax.prototype.beforeSubmit = function (values, element, options) {
-        if (this.wrapper == '#undefined') {
-          var form = $(this.selector).parents('form');
-          if (form.length) {
-            this.wrapper = '#'+form[0].id;
+        var $items = $('.media-item:not(.os-files-processed)', ctx),
+            forms = {};
+        
+        $items.each(function () {
+          var base = this.id,
+              fid = this.getAttribute('data-fid'),
+              settings = {
+                url: s.basePath+('pathPrefix' in s?s.pathPrefix:'')+'file/'+fid+'/edit/nojs',
+                event: 'click',
+                wrapper: 'file-edit-section',
+                method: 'html',
+                progress: {type: 'throbber'}
+              },
+              ajax = new Drupal.ajax(base, this, settings);
+         $(this).addClass('os-files-processed');
+        });
+        
+        // Sometimes the ajax handler will just try to work with #undefined, which is wrong
+        Drupal.ajax.prototype.beforeSubmit = function (values, element, options) {
+          if (this.wrapper == '#undefined') {
+            var form = $(this.selector).parents('form');
+            if (form.length) {
+              this.wrapper = '#'+form[0].id;
+            }
           }
-        }
-      };
-      
+        };
+      }
+        
       // makes the browser open the current tab by default when the form is reloaded
       $('#media-browser-tabset').tabs('option', 'select', function (event, ui) {
         if (ui.tab.hash) {
