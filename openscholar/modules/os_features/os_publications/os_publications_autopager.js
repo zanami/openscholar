a1099ebdbac31aa711fce543cccd4ff7f1f16c03
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-05-06 16:56:04 -0400

    Fixed redundant headers in inf scroll $2680
    Used autopager's load callback to strip out redundant instances of a selector.  I think this is fine for os_publications, but I'd like to clean it up if we generalize and use this js elsewhere.

--- openscholar/modules/os_features/os_publications/os_publications_autopager.js
+++ openscholar/modules/os_features/os_publications/os_publications_autopager.js
@@ -11,10 +11,28 @@
     attach: function (context, settings) {
       if (typeof(jQuery.autopager) == 'function') {
         for (var mod in Drupal.settings.autopager) {
-          var $pager = $.autopager(Drupal.settings.autopager[mod]);
+          var settings = Drupal.settings.autopager[mod]
+          
+          //make functions of js strings for callbacks
+          settings.load = Drupal.behaviors.os_publications.hide_redundant;
+          
+          console.log(settings);
+          var $pager = $.autopager(settings);
         }
         //hide the pager?
       }
+    },
+    
+    
+    //only keep the first of each fo the selectors.  menat for biblio loading several 2010 pages
+    hide_redundant: function() {
+      selector = '.biblio-separator-bar';
+      $selectors = $(selector);
+        
+      $selectors.each(function(selectors) {
+        html = $(this).html()
+        $selectors.filter(':contains("'+html+'"):not(:first)').remove() //filter(':contains("'+html+'")').remove();
+      });
     }
       
   }
