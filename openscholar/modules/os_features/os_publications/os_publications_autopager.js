b3dee044f81477fd0af2ee758a9bb3e28e0ca80c
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-05-08 15:06:15 -0400

    Scrolled pages get contextual links  #2680
    /publications wasn't getting contextual links on the ajax loaded pages.  I had to call Drupal.attachBehaviors to get those back.  Doing that tried to initialize the autopager again and that broke permalinks, so I wrapped the whole thing in a .once.  Now ajax loaded pages have contextual gears and the permalink updates again.

--- openscholar/modules/os_features/os_publications/os_publications_autopager.js
+++ openscholar/modules/os_features/os_publications/os_publications_autopager.js
@@ -9,74 +9,75 @@
   
   Drupal.behaviors.os_publications = {
     attach: function (context, settings) {
-      if (typeof(jQuery.autopager) == 'function') {
-        for (var mod in Drupal.settings.autopager) {
-          
-          // load and start should be able to call several small functions instead of requiring us to bloat a single function
-          function _load() {
-            for (var f in settings._load) {
-              if ((typeof(settings._load[f]) == 'function') && (settings._load[f] != _load)){
-                settings._load[f]();
+      $('body').once('os_publications_autopage', function() {
+        if (typeof(jQuery.autopager) == 'function') {
+          for (var mod in Drupal.settings.autopager) {
+            
+            // load and start should be able to call several small functions instead of requiring us to bloat a single function
+            function _load() {
+              for (var f in settings._load) {
+                if ((typeof(settings._load[f]) == 'function') && (settings._load[f] != _load)){
+                  settings._load[f]();
+                }
               }
+              return true;
             }
-            return true;
-          }
-
-          
-          function _start() {
-            for (var f in settings._start) {
-              if ((typeof(settings._start[f]) == 'function') && (settings._start[f] != _start)){
-                settings._start[f]();
+  
+            
+            function _start() {
+              for (var f in settings._start) {
+                if ((typeof(settings._start[f]) == 'function') && (settings._start[f] != _start)){
+                  settings._start[f]();
+                }
               }
+              return true;
             }
-            return true;
+            
+            //show and hide the thobber while ajax is loading
+            function show_throbber(context) {
+              $('.jquery-autopager-ajax-loader').remove();
+              $(settings.content + ':last').after(img);
+            }
+            
+            function hide_throbber() {
+              $('.jquery-autopager-ajax-loader').remove();
+            }
+            
+            //hide redundant biblio headers
+            function os_publications_hide_biblio_category_bar() {
+              selector = '.biblio-separator-bar';
+            
+              $selectors = $(selector);
+                
+              $selectors.each(function(selectors) {
+                html = $(this).html()
+                $selectors.filter(':contains("'+html+'"):not(:first)').remove() //filter(':contains("'+html+'")').remove();
+              });
+            }
+                      
+            //prep settings
+            var settings = Drupal.settings.autopager[mod]
+            if (settings.loading_image) {
+              var img = '<div class="jquery-autopager-ajax-loader" style="text-align:center;"><img src="' + settings.loading_image + '" alt="loading..."/></div>';
+            }
+  
+            settings._load = (settings.hide_biblio_categories) ? [hide_throbber, os_publications_hide_biblio_category_bar, Drupal.attachBehaviors] : [hide_throbber, Drupal.attachBehaviors];         
+            settings.load = _load;
+            
+            settings._start = [show_throbber]; 
+            settings.start = _start;
+            
+            //init
+            var $pager = $.autopager(settings);
           }
           
-          //show and hide the thobber while ajax is loading
-          function show_throbber() {
-            $('.jquery-autopager-ajax-loader').remove();
-            $(settings.content + ':last').after(img);
+          //hide the pagers
+          if (settings.pager) {
+            $(settings.pager).hide();
           }
-          
-          function hide_throbber() {
-            $('.jquery-autopager-ajax-loader').remove();
-            //Drupal.attachBehaviors(this);
-          }
-          
-          //hide redundant biblio headers
-          function os_publications_hide_biblio_category_bar() {
-            selector = '.biblio-separator-bar';
-          
-            $selectors = $(selector);
-              
-            $selectors.each(function(selectors) {
-              html = $(this).html()
-              $selectors.filter(':contains("'+html+'"):not(:first)').remove() //filter(':contains("'+html+'")').remove();
-            });
-          }
-                    
-          //prep settings
-          var settings = Drupal.settings.autopager[mod]
-          if (settings.loading_image) {
-            var img = '<div class="jquery-autopager-ajax-loader" style="text-align:center;"><img src="' + settings.loading_image + '" alt="loading..."/></div>';
-          }
-
-          settings._load = (settings.hide_biblio_categories) ? [hide_throbber, os_publications_hide_biblio_category_bar] : [hide_throbber];         
-          settings.load = _load;
-          
-          settings._start = [show_throbber]; 
-          settings.start = _start;
-          
-          //init
-          var $pager = $.autopager(settings);
         }
-        
-        //hide the pagers
-        if (settings.pager) {
-          $(settings.pager).hide();
-        }
-      }
-    },      
+      });
+    }, 
   }
   
 })(jQuery);
