3ae963d105db54a20ddac810b353ed7bffb680e7
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-05-31 17:06:53 -0400

    #2881: Add browser template. Add filter to reduce files list when search string entered.

--- openscholar/modules/os/modules/os_files/os_files_rest/misc/MediaBrowserModule.js
+++ openscholar/modules/os/modules/os_files/os_files_rest/misc/MediaBrowserModule.js
@@ -6,79 +6,91 @@
   /**
    * Service to maintain the list of files on a user's site
    */
-    factory('FileService', ['$rootScope', '$http', function ($rootScope, $http) {
-      var files = [],
-        api = {
-        add: function (file) {
-          files.push(file);
-          $rootScope.$broadcast('FileService.changed', files);
-        },
-        edit: function (file) {
-          for (var i = 0; i < files.length; i++) {
-            if (this.files[i].fid == file.fid) {
-              this.files[i] = file;
-              $rootScope.$broadcast('FileService.changed', files);
-              return true;
-            }
-          }
-          return false;
-        },
-        delete: function (fid) {
-          if (angular.isObject(fid)) {
-            fid = fid.fid;
-          }
-          for (var i = 0; i < files.length; i++) {
-            if (this.files[i].fid == fid) {
-              this.files.splice(i, 1);
-              $rootScope.$broadcast('FileService.changed', files);
-              return true;
-            }
-          }
-          return false;
-        }
-      };
-
-      $http.get({url: restPath}).
-        success(function success(data) {
-          ret.files = data.list;
-          $rootScope.$broadcast('FileService.changed', files);
-        }).
-        error(function errorFunc() {
-          $http.get({url: restPath}).
-            success(success).
-            error(errorFunc);
-        });
-
-      return api;
-    }]).
-    config(['$routeProvider', function($routeProvider) {
-      $routeProvider.
-        when('/upload', {
-          templateUrl: path+'/templates/upload.html',
-          controller: UploadCtrl
-        }).
-        when('/internet', {
-          templateUrl: path+'/templates/internet.html',
-          controller: InternetCtrl
-        }).
-        when('/browser', {
-          templateUrl: path+'/templates/browser.html',
-          controller: BrowserCtrl
-        }).
-        when('/edit/:fileId', {
-          templateUrl: path+'/templates/fileEdit.html',
-          controller: FileEditCtrl
-        }).
-        otherwise({
-          redirectTo: '/browser'
-        });
-    }]).
-  controller('BrowserCtrl', ['FileService', $scope, function (FileService, $scope) {
-      $scope.files = FileService.file;
-
-      $scope.$on('FileService.changed', function (event, files) {
-        $scope.files = files;
+  factory('FileService', ['$rootScope', '$http', function ($rootScope, $http) {
+    var files = [];
+    $http.get({url: restPath})
+      .success(function success(data) {
+        ret.files = data.list;
+        $rootScope.$broadcast('FileService.changed', files);
+      })
+      .error(function errorFunc() {
+        $http.get({url: restPath}).
+          success(success).
+          error(errorFunc);
       });
-    }]);
+
+    return {
+      add: function (file) {
+        files.push(file);
+        $rootScope.$broadcast('FileService.changed', files);
+      },
+      edit: function (file) {
+        for (var i = 0; i < files.length; i++) {
+          if (files[i].fid == file.fid) {
+            files[i] = file;
+            $rootScope.$broadcast('FileService.changed', files);
+            return true;
+          }
+        }
+        return false;
+      },
+      delete: function (fid) {
+        if (angular.isObject(fid)) {
+          fid = fid.fid;
+        }
+        for (var i = 0; i < files.length; i++) {
+          if (files[i].fid == fid) {
+            files.splice(i, 1);
+            $rootScope.$broadcast('FileService.changed', files);
+            return true;
+          }
+        }
+        return false;
+      }
+    };
+  }]).
+  /**
+   * Sets up paths for when the
+   */
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/browser/upload', {
+        templateUrl: path+'/templates/upload.html',
+        controller: UploadCtrl
+      }).
+      when('browser/internet', {
+        templateUrl: path+'/templates/internet.html',
+        controller: InternetCtrl
+      }).
+      when('/browser/list', {
+        templateUrl: path+'/templates/browser.html',
+        controller: BrowserCtrl
+      }).
+      when('/browser/edit/:fileId', {
+        templateUrl: path+'/templates/fileEdit.html',
+        controller: FileEditCtrl
+      }).
+      when('/', {}).    // do nothing when the browser isn't open
+      otherwise({
+        redirectTo: '/'
+      });
+  }]).
+  controller('BrowserCtrl', ['FileService', '$scope', function (FileService, $scope) {
+    $scope.files = FileService.file;
+
+    $scope.$on('FileService.changed', function (event, files) {
+      $scope.files = files;
+    });
+
+    $scope.queryFilename = function (item) {
+      if (item.name.indexOf($scope.filename) > -1) {
+        return true;
+      }
+      else if (item.orig.indexOf($scope.filename) > -1) {
+        return true;
+      }
+      return false;
+    }
+  }]);
 
 })();
