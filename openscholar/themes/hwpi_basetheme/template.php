4b26557cbefe2d0ab55b78e898615fb87bd34f68
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-06-06 11:19:49 -0400

    #2816: Move code adding css files to hwpi_basetheme.

--- openscholar/themes/hwpi_basetheme/template.php
+++ openscholar/themes/hwpi_basetheme/template.php
@@ -63,6 +63,15 @@ function hwpi_basetheme_page_alter(&$page) {
       )
     )
   );
+  
+  if (context_isset('context', 'os_public') && !variable_get('disable_responsive', false)) {
+    $path = drupal_get_path('theme', 'hwpi_basetheme').'/css/';
+    drupal_add_css($path.'responsive.base.css');
+    drupal_add_css($path.'responsive.layout.css');
+    drupal_add_css($path.'responsive.nav.css');
+    drupal_add_css($path.'responsive.slideshow.css');
+    drupal_add_css($path.'responsive.widgets.css');
+  }
 }
 
 /**
