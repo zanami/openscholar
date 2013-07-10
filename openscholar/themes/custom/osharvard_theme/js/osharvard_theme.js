4829b6ae92aca4961f9083d722c1f8f8095a1ae5
Dan Cabral <rdancabral@gmail.com>
2013-07-10 16:30:02 -0400

    os harvard theme updates.#3193

--- openscholar/themes/custom/osharvard_theme/js/osharvard_theme.js
+++ openscholar/themes/custom/osharvard_theme/js/osharvard_theme.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function() {
-    jQuery('.block-boxes-os_boxes_html:eq(1)').addClass('whyuse custhtml');
+	jQuery('.block-boxes-os_boxes_html:eq(1)').addClass('custhtml getsite');
+    jQuery('.block-boxes-os_boxes_html:eq(2)').addClass('custhtml whyuse');
+    jQuery('.block-boxes-os_boxes_html:eq(3)').addClass('custhtml sample');
+    jQuery('.block-boxes-os_boxes_html:eq(4)').addClass('custhtml about');
    });
