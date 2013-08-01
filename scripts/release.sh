7651a456cc28b95ba37f186164733055ac94b1b0
oren <get.oren@gmail.com>
2013-08-01 15:16:45 -0400

    allow git tag parameter in release.sh #3380

--- scripts/release.sh
+++ scripts/release.sh
@@ -29,50 +29,65 @@ then
     VERSION=$1
 fi
 
+# Finds the latest version tag
+# First, if no git tag was specified, find the major release from the info file.
 if [ -z $TAGNAME ]
 then
     VERSION_INFO=`grep "version = 7.x" openscholar/openscholar.info`
     VERSION=`echo $VERSION_INFO | sed -n 's/version = 7\.x-3\.\(.*\)/\1/p'`
 fi
-# Confirms to the user that the correct version is being used.
 
+# Next, we'll verify that a tag exists matching either the specified git tag,
+# or if none specified, tries to find the highest hotfix version for this major
+# release.
+if [ -z $TAGNAME ]
+then
+    MATCH="SCHOLAR-3.$VERSION"
+else
+    MATCH="$TAGNAME"
+fi
+TAGS=$(git tag)
+TAGNAME=$(echo "$TAGS" | (tail -r | (grep $MATCH -m1)))
+if [ -z $TAGNAME ]
+then
+  echo "No tag found matching $MATCH."
+  exit 0
+fi
+echo "Tag found:        $TAGNAME"
+if [[ $TAGNAME =~ "SCHOLAR-3\.(\d+)\.\d+" ]]
+then
+  echo "MAJOR: ${BASH_REMATCH[1]}"
+fi
+echo "$TAGNAME" | grep -Ei 'SCHOLAR-3\.(\d+)\.\d+'
+MAJOR=$(echo $TAGNAME | (sed -E 's/SCHOLAR-3\.([0-9]+)\.[0-9]+|.*/\1/'))
+MINOR=$(echo $TAGNAME | (sed -E 's/SCHOLAR-3\.[0-9]+\.([0-9]+)|.*/\1/'))
+DIRNAME="drupal-openscholar-7.x-3.$MAJOR.$MINOR"
+echo "Package filename: $DIRNAME.tar.gz"
+
+# Confirms to the user that the correct version is being used.
 echo " "
-# @todo use the TAGNAME if available.
-echo "In order to compress OpenScholar 7.x-3.${VERSION},"
-echo "this script will:"
+echo " "
+echo "IMPORTANT! this script will:"
 echo " - DESTROY any existing built local docroot"
 echo " - RESET any uncommitted changes you have made to your local git clone"
 echo " "
-echo "Note: To build a different version, either specify a different tag name:
+echo "Note: To build a different version, either specify a different git tag:"
 echo " "
-echo "    ./scripts/release.sh -t TAGNAME"
+echo "    ./scripts/release.sh TAG"
 echo " "
 echo "Or check out a different branch, edit openscholar.info and correct the version number."
 echo " "
-read -p "Continue? (y/n) "
+read -p 'Continue? (y/n) '
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
 fi
 
-# Finds the latest version tag
-# @todo use the TAGNAME if available
-if [ -z $TAGNAME ]
-then
-    MATCH="SCHOLAR-3.$VERSION"
-    TAGS=$(git tag)
-    TAGNAME=$(echo "$TAGS" | (grep $MATCH -m1))
-    if [ -z $TAGNAME ]
-    then
-      echo "No tag found mathching $MATCH."
-      exit 0
-    fi
-fi
 echo "Checking out tag $TAGNAME and running build script..."
 
 # Stores the original branch to restore after process.
 ORIG_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
-ORIG_BRANCH="(unnamed branch)"     # detached HEAD
+ORIG_BRANCH='(unnamed branch)'     # detached HEAD
 ORIG_BRANCH=${ORIG_BRANCH##refs/heads/}
 
 # Checks out the latest version tag for the detected minor version.
@@ -81,7 +96,7 @@ git checkout $TAGNAME >/dev/null 2>&1
 # Removes any existing docroots or release builds.
 rm -rf www #sudo?
 rm -rf docroot #sudo?
-DIRNAME="openscholar-7.x-3.$VERSION"
+
 echo "Building tag $TAGNAME to directory $DIRNAME..."
 if [ -d $DIRNAME ]
 then
@@ -92,7 +107,7 @@ fi
 bash scripts/build >/dev/null 2>&1
 if [ -d www ]
 then
-	mv www $DIRNAME
+  mv www $DIRNAME
 fi
 
 # Removes unnecessary 1.5M of test files
@@ -124,6 +139,6 @@ git checkout $ORIG_BRANCH >/dev/null 2>&1
 git reset --hard HEAD
 
 echo " "
-echo "Success: $DIRNAME.tar.gz ($FILESIZE)"
+echo "Success: $DIRNAME.tar.gz -- $FILESIZE"
 echo " "
 exit
