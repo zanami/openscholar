fc06411ef5589e3ffee21b19e7931abe0c1a444f
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-09-23 11:58:41 -0400

    #3868: Try to use existing AJAX to start crop tool

--- openscholar/modules/os/modules/os_files/theme/os_files.crop.js
+++ openscholar/modules/os/modules/os_files/theme/os_files.crop.js
@@ -22,8 +22,25 @@ Drupal.behaviors.osFilesImageCropBrowser = {
         // remove cropping html
       }
       else {
-        var file = selected[0];
-        console.log(e);
+        var file = selected[0],
+            element_settings = {},
+            id = $(this).attr('id');
+
+        // Ajax submits specified in this manner automatically submit to the
+        // normal form action.
+        element_settings.url = $(this.form).attr('action');
+        // Form submit button clicks need to tell the form what was clicked so
+        // it gets passed in the POST request.
+        element_settings.setClick = true;
+        // Form buttons use the 'click' event rather than mousedown.
+        element_settings.event = 'change';
+        // Clicked form buttons look better with the throbber than the progress bar.
+        element_settings.progress = { 'type': 'throbber' };
+
+        var root = $(this).parents('.form-wrapper')
+
+        var base = $(this).attr('id');
+        Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
       }
     }, settings);
     return false;
