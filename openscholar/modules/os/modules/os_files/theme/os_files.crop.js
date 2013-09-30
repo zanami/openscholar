e59c35eb31fbbd96e8420aa6a08d624d86f0d475
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-09-30 15:17:17 -0400

    #3868: Fix bugs with removing and adding new image again.

--- openscholar/modules/os/modules/os_files/theme/os_files.crop.js
+++ openscholar/modules/os/modules/os_files/theme/os_files.crop.js
@@ -7,26 +7,21 @@
 
 Drupal.behaviors.osFilesImageCropBrowser = {
   attach: function (ctx) {
-    $('.media-button', ctx).each(function () {
+    $('.media-button', ctx).once('os-files-crop').each(function () {
       var element_settings = {},
         id = $(this).attr('id'),
         target = $(this).parent().find('[name$="[fid]"]');
 
       target.attr('id', id.replace('selected-file', 'fid'));
 
-      // Ajax submits specified in this manner automatically submit to the
-      // normal form action.
       element_settings.url = $(this).attr('data-ajax_url');
-      // Form submit button clicks need to tell the form what was clicked so
-      // it gets passed in the POST request.
-      element_settings.setClick = true;
-      // Form buttons use the 'click' event rather than mousedown.
       element_settings.event = 'change';
-      element_settings.wrapper = id.replace('selected-file', 'ajax-wrapper');
+      element_settings.wrapper = $(this).parents('.form-type-managed-file').parent().attr('id');
       // Clicked form buttons look better with the throbber than the progress bar.
       element_settings.progress = { 'type': 'throbber' };
       element_settings.submit = {
         js: true,
+        // force the validator to only look at the file element
         _triggering_element_name: target.attr('name').replace('[fid]', '')
       };
 
@@ -44,17 +39,17 @@ Drupal.behaviors.osFilesImageCropBrowser = {
       types: ['image']
     },
       self = this;
+
     Drupal.media.popups.mediaBrowser(function (selected) {
       // construct the html fragment and put it in place
-      console.log(selected);
       if (selected.length == 0) {
         // remove cropping html
       }
       else {
         var file = selected[0],
-          id = $(self).attr('id').replace('selected-file', '');
+          id = $(self).attr('id');
 
-        $('#'+id+'fid').val(file.fid).change();
+        $('#'+id.replace('selected-file', 'fid')).val(file.fid).change();
       }
     }, settings);
     return false;
