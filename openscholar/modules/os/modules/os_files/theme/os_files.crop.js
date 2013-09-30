cd8b9980e1531708747d0a1b626e6372e41e2dcd
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-09-30 14:05:11 -0400

    #3868: Form saves with no errant validation errors.

--- openscholar/modules/os/modules/os_files/theme/os_files.crop.js
+++ openscholar/modules/os/modules/os_files/theme/os_files.crop.js
@@ -7,7 +7,35 @@
 
 Drupal.behaviors.osFilesImageCropBrowser = {
   attach: function (ctx) {
-    $('.media-button', ctx).click(mediaButtonClick);
+    $('.media-button', ctx).each(function () {
+      var element_settings = {},
+        id = $(this).attr('id'),
+        target = $(this).parent().find('[name$="[fid]"]');
+
+      target.attr('id', id.replace('selected-file', 'fid'));
+
+      // Ajax submits specified in this manner automatically submit to the
+      // normal form action.
+      element_settings.url = $(this).attr('data-ajax_url');
+      // Form submit button clicks need to tell the form what was clicked so
+      // it gets passed in the POST request.
+      element_settings.setClick = true;
+      // Form buttons use the 'click' event rather than mousedown.
+      element_settings.event = 'change';
+      element_settings.wrapper = id.replace('selected-file', 'ajax-wrapper');
+      // Clicked form buttons look better with the throbber than the progress bar.
+      element_settings.progress = { 'type': 'throbber' };
+      element_settings.submit = {
+        js: true,
+        _triggering_element_name: target.attr('name').replace('[fid]', '')
+      };
+
+      var root = $(this).parents('.form-wrapper');
+
+      var base = target.attr('id');
+      Drupal.ajax[base] = new Drupal.ajax(base, target[0], element_settings);
+
+    }).click(mediaButtonClick);
   }
 }
 
@@ -24,30 +52,9 @@ Drupal.behaviors.osFilesImageCropBrowser = {
       }
       else {
         var file = selected[0],
-            element_settings = {},
-            id = $(self).attr('id'),
-            target = $(self).parent().find('[name$="[fid]"]');
+          id = $(self).attr('id').replace('selected-file', '');
 
-        target.attr('id', id.replace('selected-file', 'fid'));
-
-        // Ajax submits specified in this manner automatically submit to the
-        // normal form action.
-        element_settings.url = $(self).attr('data-ajax_url');
-        // Form submit button clicks need to tell the form what was clicked so
-        // it gets passed in the POST request.
-        element_settings.setClick = true;
-        // Form buttons use the 'click' event rather than mousedown.
-        element_settings.event = 'change';
-        element_settings.wrapper = id.replace('selected-file', 'ajax-wrapper');
-        // Clicked form buttons look better with the throbber than the progress bar.
-        element_settings.progress = { 'type': 'throbber' };
-
-        var root = $(self).parents('.form-wrapper')
-
-        var base = target.attr('id');
-        Drupal.ajax[base] = new Drupal.ajax(base, target[0], element_settings);
-
-        target.val(file.fid).change();
+        $('#'+id+'fid').val(file.fid).change();
       }
     }, settings);
     return false;
