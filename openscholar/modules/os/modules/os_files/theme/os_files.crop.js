da74188b37cb9e8447ea1b5c0f3067d6ff6b47bb
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-09-30 10:14:05 -0400

    #3868: Crop form loads and appears to be functional.

--- openscholar/modules/os/modules/os_files/theme/os_files.crop.js
+++ openscholar/modules/os/modules/os_files/theme/os_files.crop.js
@@ -14,7 +14,8 @@ Drupal.behaviors.osFilesImageCropBrowser = {
   function mediaButtonClick(e) {
     var settings = {
       types: ['image']
-    };
+    },
+      self = this;
     Drupal.media.popups.mediaBrowser(function (selected) {
       // construct the html fragment and put it in place
       console.log(selected);
@@ -24,23 +25,29 @@ Drupal.behaviors.osFilesImageCropBrowser = {
       else {
         var file = selected[0],
             element_settings = {},
-            id = $(this).attr('id');
+            id = $(self).attr('id'),
+            target = $(self).parent().find('[name$="[fid]"]');
+
+        target.attr('id', id.replace('selected-file', 'fid'));
 
         // Ajax submits specified in this manner automatically submit to the
         // normal form action.
-        element_settings.url = $(this.form).attr('action');
+        element_settings.url = $(self).attr('data-ajax_url');
         // Form submit button clicks need to tell the form what was clicked so
         // it gets passed in the POST request.
         element_settings.setClick = true;
         // Form buttons use the 'click' event rather than mousedown.
         element_settings.event = 'change';
+        element_settings.wrapper = id.replace('selected-file', 'ajax-wrapper');
         // Clicked form buttons look better with the throbber than the progress bar.
         element_settings.progress = { 'type': 'throbber' };
 
-        var root = $(this).parents('.form-wrapper')
+        var root = $(self).parents('.form-wrapper')
 
-        var base = $(this).attr('id');
-        Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
+        var base = target.attr('id');
+        Drupal.ajax[base] = new Drupal.ajax(base, target[0], element_settings);
+
+        target.val(file.fid).change();
       }
     }, settings);
     return false;
