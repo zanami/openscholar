bc42e0afce157d51fbdcc2c3c487471d4fc64134
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-10-02 16:31:57 -0400

    #3845: Add js to toggle show/hide state. Save that state in menu_link

---
+++ openscholar/modules/os_features/os_pages/os_pages.outline.js
@@ -0,0 +1,23 @@
+/**
+ * Handles toggling of the visibility parameter
+ */
+(function ($) {
+Drupal.behaviors.osPagesOutline = {
+  attach: function (ctx) {
+    $('.visibility-toggle-link', ctx).click(toggleVisibility);
+  }
+}
+
+function toggleVisibility(e) {
+  var input = $(e.target).parent().find('input');
+
+  input.val(Math.abs(input.val()-1));
+  if (parseInt(input.val())) {
+    e.target.innerHTML = Drupal.t('Show');
+  }
+  else {
+    e.target.innerHTML = Drupal.t('Hide');
+  }
+  return false;
+}
+})(jQuery)
