94fda04fd9611c4da1edd86206772f70bf5fc26b
Matt Petrovic <mpetrovic@iq.harvard.edu>
2013-10-03 16:50:29 -0400

    #3845: Toggle visibility on all children links as well.
    This happened automatically, but could be confusing since the UI didn't state this.

--- openscholar/modules/os_features/os_pages/os_pages.outline.js
+++ openscholar/modules/os_features/os_pages/os_pages.outline.js
@@ -18,6 +18,17 @@ function toggleVisibility(e) {
   else {
     e.target.innerHTML = Drupal.t('Show');
   }
+
+  toggleChildren(input.parents('tr').find('.book-mlid').val());
   return false;
+}
+
+function toggleChildren(mlid) {
+  $('.book-plid[value='+mlid+']').each(function () {
+    var hidden = $(this).parents('tr').find('.visibility-toggle-link + input').get(0);
+    hidden.value = Math.abs(hidden.value-1);
+    $(this).parents('tr').find('.visibility-toggle-link').html(parseInt(hidden.value)?Drupal.t('Hide'):Drupal.t('Show'));
+    toggleChildren($(this).parent().find('.book-mlid').val());
+  });
 }
 })(jQuery)
