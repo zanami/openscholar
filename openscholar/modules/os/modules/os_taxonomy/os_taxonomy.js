6df5fe25d124b29deef454d95ad9a7acd2578347
Ran Rosenberg <ran@gizra.com>
2013-10-10 10:50:31 +0300

    Fix comments.

--- openscholar/modules/os/modules/os_taxonomy/os_taxonomy.js
+++ openscholar/modules/os/modules/os_taxonomy/os_taxonomy.js
@@ -2,10 +2,8 @@
 
   Drupal.behaviors.submitOnChange = {
     attach: function () {
-      $(function() {
-        $('.terms-list').change(function(e) {
-          $(this).closest('form').trigger('submit')
-        })
+      $('.terms-list').change(function(e) {
+        $(this).closest('form').trigger('submit')
       })
     }
   };
