7d46b5d49eac9f6e4c193f8ecacdfc9e134f5120
oren <get.oren@gmail.com>
2013-10-16 14:43:52 -0400

    add function to format an individual tour step item #3951

--- openscholar/modules/os/theme/os_notifications.js
+++ openscholar/modules/os/theme/os_notifications.js
@@ -30,4 +30,42 @@
 
     }
   };
+
+  /**
+   * Renders a feed object into an HTML snippet.
+   *
+   * @param {object} entry
+   * @returns {string} output
+   */
+  function os_notifications_item(entry) {
+    var output = "<div class='feed_item'>";
+    var date = "";
+    if (typeof entry.publishedDate != 'undefined' && entry.publishedDate != '') {
+      date = os_notifications_fuzzy_date(entry.publishedDate);
+      if (typeof date == 'undefined') {
+        date = "";
+      } else {
+        date = "<span class='date'>" + date + "</span>";
+      }
+    }
+    output += date;
+    output += "<span class='description'>" + content + "<span/>";
+
+    //
+    var content = entry.content;
+    if (typeof entry.contentSnippet != 'undefined') {
+      content = entry.contentSnippet;
+    }
+    output += content;
+    output += "<br/><a class='title' target='_blank' href='" + entry.link + "'>Read more &raquo;</a></div>";
+
+    var item = {
+      title: entry.title,
+      content:output,
+      target: document.querySelector("#os-notifications-list"),
+      placement: "bottom",
+      yOffset: 20
+    };
+    return item;
+  }
 })(jQuery);
