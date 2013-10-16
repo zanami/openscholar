e11d5ab1c5b4f8d770e645dd8525bd0ca0898438
oren <get.oren@gmail.com>
2013-10-16 14:43:10 -0400

    fetch feed items with google feeds api v1 #3951

--- openscholar/modules/os/theme/os_notifications.js
+++ openscholar/modules/os/theme/os_notifications.js
@@ -8,6 +8,25 @@
       // Setup.
       var menuLinkSel = '#os-notifications-menu-link';
       $(menuLinkSel).attr('href', '#').parent('li').append($("<div id='os-notifications-list'/>"));
+      var settings = Drupal.settings.os_notifications;
+      var container = $('#os-notifications-list');
+      if (typeof google == 'undefined') {
+        return;
+      }
+      // @TODO: Add support for multiple feeds.
+      var feed = new google.feeds.Feed(settings.url);
+      var items = [];
+      feed.setNumEntries(settings.max);
+      feed.load(function (result) {
+        if (!result.error) {
+          for (var i = 0; i < result.feed.entries.length; i++) {
+            var entry = result.feed.entries[i];
+            var item = os_notifications_item(entry);
+            items.push(item);
+            console.log(item);
+          }
+        }
+      });
 
     }
   };
