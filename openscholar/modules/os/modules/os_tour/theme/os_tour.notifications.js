704f155070fc594964a0c152c40140ff7ab76b36
oren <get.oren@gmail.com>
2013-10-22 14:38:50 -0400

    add AJAX callback and test for logic update #3951

--- openscholar/modules/os/modules/os_tour/theme/os_tour.notifications.js
+++ openscholar/modules/os/modules/os_tour/theme/os_tour.notifications.js
@@ -45,6 +45,7 @@
         // Allows us to target just this tour in CSS rules.
         $('.hopscotch-bubble').addClass('os-tour-notifications');
       });
+      $.get('os/tour/notifications-read/' + parseInt(Drupal.cookie("DRUPAL_UID")));
     }
   };
 
