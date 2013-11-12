bf305b7906c32acaa183c515e14cc23c1963237c
RoySegall <roy.segall@gizra.com>
2013-11-12 16:27:59 +0200

    Using theme instead of meessage.

---
+++ openscholar/modules/os/theme/openscholar-get-started.tpl.php
@@ -0,0 +1,8 @@
+<div id="message_welcome_message">
+  <div class="toggle"><?php print $short_message; ?></div>
+    <div id="welcome_wrapper" class="os-slider">
+    <h3><?php print $welcome_message; ?></h3>
+    <p><?php print $below_message; ?></p>
+    <?php print $items; ?>
+  </div> <!-- SLIDER -->
+</div>
