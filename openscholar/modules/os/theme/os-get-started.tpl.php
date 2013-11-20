9fb1df57dac0b28bf0fea8a57e5e04f42df7b7c2
Amitaibu <amitai@gizra.com>
2013-11-20 13:45:31 +0200

    Start working on theme.

--- openscholar/modules/os/theme/os-get-started.tpl.php
+++ openscholar/modules/os/theme/os-get-started.tpl.php
@@ -1,3 +1,24 @@
+<div class="top-wrapper clearfix">
+  <?php print $os_logo; ?>
+  <div class="tagline"><?php print $tagline; ?></div>
+  <div class="first-wrapper">
+    <?php print render($search_block); ?>
+  </div>
+  <div class="last-wrapper">
+    <div class="btn-wrapper">
+      <?php print $create_link; ?>
+    </div>
+  </div>
+</div>
+
+<div class="middle-wrapper">
+
+</div>
+
+<div class="bottom-wrapper">
+
+</div>
+
 <div id="message_welcome_message">
   <div class="toggle"><?php print $short_message; ?></div>
     <div id="welcome_wrapper" class="os-slider">
